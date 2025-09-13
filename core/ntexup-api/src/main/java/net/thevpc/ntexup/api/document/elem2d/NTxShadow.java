a86dc8387bb9900f6bf93ac6647aab2671f46081
thevpc <taha.bensalah@gmail.com>
2025-09-13 08:42:00 +0100

    Update NTxShadow.java : fixed shadows

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem2d/NTxShadow.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem2d/NTxShadow.java
@@ -1,13 +1,27 @@
 package net.thevpc.ntexup.api.document.elem2d;
 
+import net.thevpc.nuts.util.NBlankable;
+
 import java.awt.Paint;
 import java.util.Objects;
 
-public class NTxShadow {
+public class NTxShadow implements Cloneable, NBlankable {
 
     private NTxPoint2D translation;
     private Paint color;
     private NTxPoint2D shear;
+    private NTxPoint2D zoom;
+    private double radius;
+    private double alpha;
+
+    public double getAlpha() {
+        return alpha;
+    }
+
+    public NTxShadow setAlpha(double alpha) {
+        this.alpha = alpha;
+        return this;
+    }
 
     public void setColor(Paint color) {
         this.color = color;
@@ -17,21 +31,78 @@ public class NTxShadow {
         return color;
     }
 
+    public NTxPoint2D getZoom() {
+        return zoom;
+    }
+
+    public NTxShadow setZoom(NTxPoint2D zoom) {
+        this.zoom = zoom;
+        return this;
+    }
+
     public NTxPoint2D getTranslation() {
         return translation;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public NTxShadow setRadius(double radius) {
+        this.radius = radius;
+        return this;
+    }
+
+    @Override
+    public boolean isBlank() {
+        if (translation != null) {
+            if (translation.x != 0) {
+                return false;
+            }
+            if (translation.y != 0) {
+                return false;
+            }
+        }
+        if (zoom != null) {
+            if (zoom.x != 0) {
+                return false;
+            }
+            if (zoom.y != 0) {
+                return false;
+            }
+        }
+        if (shear != null) {
+            if (shear.x != 0) {
+                return false;
+            }
+            if (shear.y != 0) {
+                return false;
+            }
+        }
+        if (!Double.isNaN(radius) && radius > 0) {
+            return false;
+        }
+        if (!Double.isNaN(alpha)) {
+            return false;
+        }
+        return false;
     }
 
     public void setTranslation(NTxPoint2D translation) {
         this.translation = translation;
     }
 
-    public NTxPoint2D getShear() {return shear;}
+    public NTxPoint2D getShear() {
+        return shear;
+    }
 
-    public void setShear (NTxPoint2D shear) {this.shear = shear;}
+    public void setShear(NTxPoint2D shear) {
+        this.shear = shear;
+    }
 
     @Override
     public String toString() {
-        return "Shadow{" + "translation=" + translation + ", color=" + color +", shear=" + shear + '}';
+        return "Shadow{" + "translation=" + translation + ", color=" + color + ", shear=" + shear + '}';
     }
 
     @Override
@@ -40,6 +111,9 @@ public class NTxShadow {
         hash = 29 * hash + Objects.hashCode(this.translation);
         hash = 29 * hash + Objects.hashCode(this.color);
         hash = 29 * hash + Objects.hashCode(this.shear);
+        hash = 29 * hash + Objects.hashCode(this.zoom);
+        hash = 29 * hash + Objects.hashCode(this.radius);
+        hash = 29 * hash + Objects.hashCode(this.alpha);
         return hash;
     }
 
@@ -61,9 +135,26 @@ public class NTxShadow {
         if (!Objects.equals(this.color, other.color)) {
             return false;
         }
-        return Objects.equals(this.shear, other.shear);
+        if (!Objects.equals(this.shear, other.shear)) {
+            return false;
+        }
+        if (!Objects.equals(this.zoom, other.zoom)) {
+            return false;
+        }
+        if (!Objects.equals(this.radius, other.radius)) {
+            return false;
+        }
+        if (!Objects.equals(this.alpha, other.alpha)) {
+            return false;
+        }
+        return true;
     }
 
-    
-
+    public NTxShadow copy() {
+        try {
+            return (NTxShadow) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
