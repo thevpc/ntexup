1700581e4bd29da709975cb50d95a05227f9d4f8
thevpc <taha.bensalah@gmail.com>
2025-09-27 08:23:00 +0100

    Update NTexupMain.java : update NTxTemplateInfo

--- app/ntexup/src/main/java/net/thevpc/ntexup/NTexupMain.java
+++ app/ntexup/src/main/java/net/thevpc/ntexup/NTexupMain.java
@@ -21,30 +21,13 @@ public class NTexupMain {
         NWorkspace.of().share();
         Options options = new Options();
         NCmdLine cmdLine = NApp.of().getCmdLine();
-        while (!cmdLine.isEmpty()) {
-            NArg p = cmdLine.peek().get();
-            if (p.isOption()) {
-                if (p.key().equals("--gui")) {
-                    NSession.of().setGui(true);
-                    cmdLine.next();
-                    new NTxViewerProcessor().runViewer(cmdLine, options);
-                    return;
-                } else if (p.key().equals("--view")) {
-                    cmdLine.next();
-                    new NTxViewerProcessor().runViewer(cmdLine, options);
-                    return;
-                } else if (NSession.of().configureFirst(cmdLine)) {
-                    // okkay
-                } else {
-                    new NTxTerminalProcessor().runTerminal(cmdLine, options);
-                    return;
-                }
-            } else {
-                new NTxTerminalProcessor().runTerminal(cmdLine, options);
-                return;
-            }
+        cmdLine.setCommandName("ntexup");
+        new NTexupOptionsParser().parse(cmdLine, options);
+        if(options.terminalMode){
+            new NTxTerminalProcessor().runTerminal(options);
+        }else{
+            new NTxViewerProcessor().runViewer(options);
         }
-        new NTxTerminalProcessor().runTerminal(cmdLine, options);
     }
 
 
