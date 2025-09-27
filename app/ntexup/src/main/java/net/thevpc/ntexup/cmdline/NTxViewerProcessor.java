ec9b7f90be7dc19815c977fda9ed9389e5116370
thevpc <taha.bensalah@gmail.com>
2025-09-27 08:23:00 +0100

    Update NTxViewerProcessor.java : update NTxTemplateInfo

--- app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/NTxViewerProcessor.java
+++ app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/NTxViewerProcessor.java
@@ -2,6 +2,7 @@ package net.thevpc.ntexup.cmdline;
 
 import com.formdev.flatlaf.FlatLightLaf;
 import net.thevpc.ntexup.main.MainFrame;
+import net.thevpc.ntexup.util.NTexupUtils;
 import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.nswing.NSwingUtils;
@@ -9,48 +10,45 @@ import net.thevpc.nuts.nswing.NSwingUtils;
 import javax.swing.*;
 
 public class NTxViewerProcessor {
-    public void runViewer(NCmdLine cmdLine,Options options) {
-        while (!cmdLine.isEmpty()) {
-            cmdLine.matcher()
-                    .with("--reopen").matchTrueFlag(a -> options.reopen = true)
-                    .with("--documentation").matchFlag(a -> options.documentation = true)
-                    .with("--open").matchEntry(a -> {
-                        if(a.getStringValue().isPresent()){
-                            options.paths.add(NPath.of(a.stringValue()));
-                        }else{
-                            options.paths.add(NPath.ofUserDirectory());
-                        }
-                    })
-                    .with("--new").matchTrueFlag(a -> options.action = Action.NEW)
-                    .with("--view").matchTrueFlag(a -> options.requireViewer())
-                    .with("--view-log").matchTrueFlag(a -> options.requireViewer().showLogs = true)
-                    .withNonOption().matchAny(a -> options.paths.add(NPath.of(a.image())))
-                    .requireDefaults();
-        }
-        if (options.documentation) {
-            options.paths.add(NPath.of("github://thevpc/ntexup-doc-slides/"));
-        }
+    public void runViewer(Options options) {
         NSwingUtils.setSharedWorkspaceInstance();
         FlatLightLaf.setup(new com.formdev.flatlaf.FlatDarculaLaf());
         MainFrame mainFrame = new MainFrame();
-        SwingUtilities.invokeLater(() -> {
+        NTexupUtils.runUiAsync(()->{
             mainFrame.setVisible(true);
-            if (options.reopen) {
-                NPath p = mainFrame.getService().getLatestProjectPath();
-                if (p != null) {
-                    options.paths.add(p);
-                }
-            }
-            if (options.showLogs) {
-                mainFrame.getService().showDebug();
-            }
-            if (options.paths.isEmpty()) {
-
-            } else {
-                for (NPath path : options.paths) {
-                    mainFrame.getService().openProject(path);
-                }
-            }
         });
+
+        if (options.reopen) {
+            NPath p = mainFrame.getService().getLatestProjectPath();
+            if (p != null) {
+                options.paths.add(p);
+            }
+        }
+        if (options.showLogs) {
+            mainFrame.getService().showDebug();
+        }
+        switch (options.action){
+            case OPEN:{
+                if (options.paths.isEmpty()) {
+
+                } else {
+                    for (NPath path : options.paths) {
+                        mainFrame.getService().openProject(path);
+                    }
+                }
+                break;
+            }
+            case NEW:{
+                if (options.paths.isEmpty()) {
+                    mainFrame.getService().showNewProject(null);
+                } else {
+                    for (NPath path : options.paths) {
+                        mainFrame.getService().showNewProject(path.toString());
+                    }
+                }
+                break;
+            }
+        }
+
     }
 }
