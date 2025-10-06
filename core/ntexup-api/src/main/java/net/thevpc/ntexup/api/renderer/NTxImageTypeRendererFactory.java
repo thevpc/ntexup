8f8d5ba1776029a306d93c614aab527d833fc587
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:22:22 +0100

    Update NTxImageTypeRendererFactory.java : bump nuts 0.8.7

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxImageTypeRendererFactory.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxImageTypeRendererFactory.java
@@ -1,7 +1,7 @@
 package net.thevpc.ntexup.api.renderer;
 
 import net.thevpc.ntexup.api.document.elem2d.NTxImageOptions;
-import net.thevpc.nuts.NCallableSupport;
+import net.thevpc.nuts.concurrent.NCallableSupport;
 import net.thevpc.nuts.io.NPath;
 
 public interface NTxImageTypeRendererFactory {
