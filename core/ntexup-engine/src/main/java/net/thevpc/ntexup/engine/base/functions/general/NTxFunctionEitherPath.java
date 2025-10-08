fb79607ad7fa5ee12edea897901c5434ffb122fa
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:48 +0100

    Update NTxFunctionEitherPath.java : refactor NMsg

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/general/NTxFunctionEitherPath.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/general/NTxFunctionEitherPath.java
@@ -9,7 +9,7 @@ import net.thevpc.ntexup.engine.eval.NTxGitHelper;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.util.NBlankable;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 public class NTxFunctionEitherPath implements NTxFunction {
     @Override
