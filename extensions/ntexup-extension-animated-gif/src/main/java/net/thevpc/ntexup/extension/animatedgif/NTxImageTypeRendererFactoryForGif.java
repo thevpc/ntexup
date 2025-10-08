fd6aac2d7e105168b986d738d697799d615c45db
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:49 +0100

    Update NTxImageTypeRendererFactoryForGif.java : refactor NMsg

--- extensions/ntexup-extension-animated-gif/src/main/java/net/thevpc/ntexup/extension/animatedgif/NTxImageTypeRendererFactoryForGif.java
+++ extensions/ntexup-extension-animated-gif/src/main/java/net/thevpc/ntexup/extension/animatedgif/NTxImageTypeRendererFactoryForGif.java
@@ -4,9 +4,9 @@ import net.thevpc.ntexup.api.document.elem2d.NTxImageOptions;
 import net.thevpc.ntexup.api.renderer.NTxImageTypeRendererFactory;
 import net.thevpc.ntexup.api.renderer.NTxGraphics;
 import net.thevpc.ntexup.api.renderer.NTxGraphicsImageDrawer;
-import net.thevpc.nuts.concurrent.NCallableSupport;
+import net.thevpc.nuts.concurrent.NScorableCallable;
 import net.thevpc.nuts.io.NPath;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,10 +16,10 @@ public class NTxImageTypeRendererFactoryForGif implements NTxImageTypeRendererFa
     private Map<String, FutureTask<NPath>> pendingCache = new HashMap<>();
 
     @Override
-    public NCallableSupport<NTxGraphicsImageDrawer> resolveRenderer(NPath path, NTxImageOptions options, NTxGraphics graphics) {
+    public NScorableCallable<NTxGraphicsImageDrawer> resolveRenderer(NPath path, NTxImageOptions options, NTxGraphics graphics) {
         if (!options.isDisableAnimation()) {
             if (path.getName().toLowerCase().endsWith(".gif")) {
-                return NCallableSupport.ofValid(
+                return NScorableCallable.ofValid(
                         () -> {
                             byte[] b = path.readBytes();
                             return new GifNTxImageDrawer(b, pendingCache);
@@ -27,6 +27,6 @@ public class NTxImageTypeRendererFactoryForGif implements NTxImageTypeRendererFa
                 );
             }
         }
-        return NCallableSupport.ofInvalid(() -> NMsg.ofC("not supported %s", path));
+        return NScorableCallable.ofInvalid(() -> NMsg.ofC("not supported %s", path));
     }
 }
