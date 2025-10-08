0ebe30a438974907d5192cef7730967b55a2b4a5
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:48 +0100

    Update CustomNamedParamAction.java : refactor NMsg

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/ext/CustomNamedParamAction.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/ext/CustomNamedParamAction.java
@@ -11,7 +11,7 @@ import net.thevpc.nuts.elem.NElementType;
 import net.thevpc.nuts.elem.NElementTypeGroup;
 import net.thevpc.nuts.elem.NPairElement;
 import net.thevpc.nuts.util.NBlankable;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 
 import java.util.*;
