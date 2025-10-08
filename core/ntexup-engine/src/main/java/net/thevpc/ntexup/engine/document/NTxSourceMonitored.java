174328fda5f95cdc52b7693e2bf9e92b7a909ea2
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:48 +0100

    Update NTxSourceMonitored.java : refactor NMsg

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/document/NTxSourceMonitored.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/document/NTxSourceMonitored.java
@@ -4,7 +4,7 @@ import net.thevpc.ntexup.api.source.NTxSource;
 import net.thevpc.ntexup.api.source.NTxSourceMonitor;
 import net.thevpc.ntexup.engine.parser.resources.NTxSourceFactory;
 import net.thevpc.nuts.io.NPath;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 
 import java.util.*;
