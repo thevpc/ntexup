2c16a4511f5f3a384c69956dac092cebe2952eab
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:49 +0100

    Update DefaultNTxNodeRendererContext.java : refactor NMsg

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/DefaultNTxNodeRendererContext.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/DefaultNTxNodeRendererContext.java
@@ -25,7 +25,7 @@ import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.util.NAssert;
 import net.thevpc.nuts.util.NLiteral;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 
 import java.awt.*;
