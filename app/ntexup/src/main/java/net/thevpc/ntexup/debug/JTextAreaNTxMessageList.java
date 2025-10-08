a054632ab0bd9ab8f105cbc4562c5ae33f1a5498
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:47 +0100

    Update JTextAreaNTxMessageList.java : refactor NMsg

--- app/ntexup/src/main/java/net/thevpc/ntexup/debug/JTextAreaNTxMessageList.java
+++ app/ntexup/src/main/java/net/thevpc/ntexup/debug/JTextAreaNTxMessageList.java
@@ -3,7 +3,7 @@ package net.thevpc.ntexup.debug;
 import net.thevpc.ntexup.api.log.NTxLogger;
 import net.thevpc.ntexup.api.log.NTxMsg;
 import net.thevpc.ntexup.api.source.NTxSource;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 import javax.swing.*;
 import java.awt.*;
