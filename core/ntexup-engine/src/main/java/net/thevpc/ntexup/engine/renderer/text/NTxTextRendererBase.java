b97157a053ae5d11d6a5589acb3fca63acc1353c
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:49 +0100

    Update NTxTextRendererBase.java : refactor NMsg

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/text/NTxTextRendererBase.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/text/NTxTextRendererBase.java
@@ -14,7 +14,7 @@ import net.thevpc.ntexup.engine.util.NTxUtilsText;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.util.NBlankable;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 import java.awt.*;
 
