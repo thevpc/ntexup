d6d852cd3e495a420c9a9aff8cf0d8dde880dba4
thevpc <taha.bensalah@gmail.com>
2025-10-09 12:13:32 +0100

    Update NTxNodeParserFactory.java : fixed

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/parser/NTxNodeParserFactory.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/parser/NTxNodeParserFactory.java
@@ -1,13 +1,13 @@
 package net.thevpc.ntexup.api.parser;
 
 import net.thevpc.ntexup.api.document.node.NTxItem;
-import net.thevpc.nuts.concurrent.NScorableCallable;
+import net.thevpc.nuts.concurrent.NScoredCallable;
 
 /**
  * @author vpc
  */
 public interface NTxNodeParserFactory {
 
-    NScorableCallable<NTxItem> parseNode(NTxNodeFactoryParseContext context);
+    NScoredCallable<NTxItem> parseNode(NTxNodeFactoryParseContext context);
 
 }
