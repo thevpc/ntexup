b31cc5d708f4fd74c9c250bc42da4b099a48f570
thevpc <taha.bensalah@gmail.com>
2025-10-09 12:13:33 +0100

    Update ScreenDocumentStreamRendererFactory.java : fixed

--- renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/ScreenDocumentStreamRendererFactory.java
+++ renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/ScreenDocumentStreamRendererFactory.java
@@ -7,7 +7,7 @@ package net.thevpc.ntexup.engine.renderer.screen;
 import net.thevpc.ntexup.api.renderer.NTxDocumentRenderer;
 import net.thevpc.ntexup.api.renderer.NTxDocumentRendererFactory;
 import net.thevpc.ntexup.api.renderer.NTxDocumentRendererFactoryContext;
-import net.thevpc.nuts.concurrent.NScorableCallable;
+import net.thevpc.nuts.concurrent.NScoredCallable;
 import net.thevpc.nuts.text.NMsg;
 
 /**
@@ -16,12 +16,12 @@ import net.thevpc.nuts.text.NMsg;
 public class ScreenDocumentStreamRendererFactory implements NTxDocumentRendererFactory {
 
     @Override
-    public NScorableCallable<NTxDocumentRenderer> createDocumentRenderer(NTxDocumentRendererFactoryContext context) {
+    public NScoredCallable<NTxDocumentRenderer> createDocumentRenderer(NTxDocumentRendererFactoryContext context) {
         switch (String.valueOf(context.rendererType()).toLowerCase()) {
             case "screen":
-                return NScorableCallable.ofValid( () -> new ScreenDocumentRenderer(context.engine()));
+                return NScoredCallable.ofValid( () -> new ScreenDocumentRenderer(context.engine()));
             default:
-                return NScorableCallable.ofInvalid(() -> NMsg.ofPlain("factory"));
+                return NScoredCallable.ofInvalid(() -> NMsg.ofPlain("factory"));
         }
     }
 
