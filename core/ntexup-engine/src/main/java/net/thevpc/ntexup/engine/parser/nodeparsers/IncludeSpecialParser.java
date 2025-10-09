3ea62fa3191ffb95655f8a2c35b338ba3cb06913
thevpc <taha.bensalah@gmail.com>
2025-10-09 12:13:33 +0100

    Update IncludeSpecialParser.java : fixed

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/nodeparsers/IncludeSpecialParser.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/nodeparsers/IncludeSpecialParser.java
@@ -6,7 +6,7 @@ import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.parser.NTxNodeFactoryParseContext;
 import net.thevpc.ntexup.api.util.NTxUtils;
 import net.thevpc.ntexup.engine.parser.ctrlnodes.CtrlNTxNodeInclude;
-import net.thevpc.nuts.concurrent.NScorableCallable;
+import net.thevpc.nuts.concurrent.NScoredCallable;
 import net.thevpc.nuts.elem.NUpletElement;
 import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.elem.NElement;
@@ -18,18 +18,18 @@ public class IncludeSpecialParser extends NTxNodeParserBase {
 
 
     @Override
-    public NScorableCallable<NTxItem> parseNode(NTxNodeFactoryParseContext context) {
+    public NScoredCallable<NTxItem> parseNode(NTxNodeFactoryParseContext context) {
         NElement tsonElement = context.element();
         switch (tsonElement.type()) {
             case NAMED_UPLET: {
                 NUpletElement uplet = tsonElement.asUplet().get();
                 if (uplet.isNamed("include")) {
-                    return NScorableCallable.ofValid( () -> new CtrlNTxNodeInclude(context.source(),uplet.params()));
+                    return NScoredCallable.ofValid( () -> new CtrlNTxNodeInclude(context.source(),uplet.params()));
                 }
                 break;
             }
         }
-        return NScorableCallable.ofInvalid(NMsg.ofC("missing include from %s", NTxUtils.snippet(tsonElement)).asError());
+        return NScoredCallable.ofInvalid(NMsg.ofC("missing include from %s", NTxUtils.snippet(tsonElement)).asError());
     }
 
 
