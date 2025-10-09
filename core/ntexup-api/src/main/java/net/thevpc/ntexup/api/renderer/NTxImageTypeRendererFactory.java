2b92a65f36a8646d026450c2411f327256198262
thevpc <taha.bensalah@gmail.com>
2025-10-09 12:13:32 +0100

    Update NTxImageTypeRendererFactory.java : fixed

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxImageTypeRendererFactory.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxImageTypeRendererFactory.java
@@ -1,9 +1,9 @@
 package net.thevpc.ntexup.api.renderer;
 
 import net.thevpc.ntexup.api.document.elem2d.NTxImageOptions;
-import net.thevpc.nuts.concurrent.NScorableCallable;
+import net.thevpc.nuts.concurrent.NScoredCallable;
 import net.thevpc.nuts.io.NPath;
 
 public interface NTxImageTypeRendererFactory {
-    NScorableCallable<NTxGraphicsImageDrawer> resolveRenderer(NPath path, NTxImageOptions options, NTxGraphics graphics);
+    NScoredCallable<NTxGraphicsImageDrawer> resolveRenderer(NPath path, NTxImageOptions options, NTxGraphics graphics);
 }
