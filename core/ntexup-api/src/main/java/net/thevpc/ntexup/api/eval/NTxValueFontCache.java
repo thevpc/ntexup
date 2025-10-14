958d2f93049e83c633fbedeea52817a1b770313e
thevpc <taha.bensalah@gmail.com>
2025-10-14 21:59:07 +0100

    Update NTxValueFontCache.java : update cmdline

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxValueFontCache.java
@@ -0,0 +1,20 @@
+package net.thevpc.ntexup.api.eval;
+
+import net.thevpc.ntexup.api.document.elem2d.*;
+import net.thevpc.ntexup.api.util.NTxSizeRef;
+import net.thevpc.ntexup.api.util.NtxFontInfo;
+
+import java.awt.*;
+
+public class NTxValueFontCache {
+    public boolean fontItalic;
+    public boolean fontStrike;
+    public boolean fontUnderline;
+    public boolean fontBold;
+    public String fontFamily;
+    public Font font;
+    public NTxSize fontSize;
+    public double fontXSize;
+    public double fontYSize;
+    public NtxFontInfo fontInfo;
+}
