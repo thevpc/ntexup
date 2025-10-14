f67c331fd953c8aa5e3a4e88e7c169f47415b880
thevpc <taha.bensalah@gmail.com>
2025-10-14 21:59:07 +0100

    Update NTxValueCommonCache.java : update cmdline

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxValueCommonCache.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxValueCommonCache.java
@@ -1,36 +1,16 @@
 package net.thevpc.ntexup.api.eval;
 
-import net.thevpc.ntexup.api.document.elem2d.*;
-import net.thevpc.ntexup.api.util.NTxSizeRef;
-import net.thevpc.ntexup.api.util.NtxFontInfo;
 import net.thevpc.nuts.elem.NElement;
 
 import java.awt.*;
 
 public class NTxValueCommonCache {
-    public Font font;
-    public NTxSize fontSize;
-    public double fontXSize;
-    public double fontYSize;
-    public boolean fontItalic;
-    public boolean fontStrike;
-    public boolean fontUnderline;
-    public boolean fontBold;
-    public String fontFamily;
 //    public boolean visible;
     public NElement stroke;
-    public NTxElemNumber2 position;
-    public NTxElemNumber2 origin;
-    public boolean preserveRatio;
-    public NtxFontInfo fontInfo;
     public Paint foregroundColor;
     public Paint backgroundColor;
-    public NTxDouble2 componentSize;
-    public NTxMargin margin;
     public boolean drawContour;
     public boolean fillBackground;
     public int debugLevel;
     public Color debugColor;
-    public NTxSizeRef parentWithMarginRef;
-    public NTxBounds2 parentBoundsWithMargin;
 }
