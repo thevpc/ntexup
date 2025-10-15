4b042fdd3438c79fc142e155e20627c8dd1fef8d
thevpc <taha.bensalah@gmail.com>
2025-10-15 14:47:07 +0100

    Update ActionOptions.java : fixed-cmdline

---
+++ app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/options/ActionOptions.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.cmdline.options;
+
+public class ActionOptions {
+    public final Action action;
+
+    public ActionOptions(Action action) {
+        this.action = action;
+    }
+}
