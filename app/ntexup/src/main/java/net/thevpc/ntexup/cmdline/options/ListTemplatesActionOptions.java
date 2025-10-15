89651be32278d28c4efda9ac230c2201179cc7f2
thevpc <taha.bensalah@gmail.com>
2025-10-15 14:47:07 +0100

    Update ListTemplatesActionOptions.java : fixed-cmdline

---
+++ app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/options/ListTemplatesActionOptions.java
@@ -0,0 +1,7 @@
+package net.thevpc.ntexup.cmdline.options;
+
+public class ListTemplatesActionOptions extends ActionOptions {
+    public ListTemplatesActionOptions() {
+        super(Action.LIST_TEMPLATES);
+    }
+}
