8953fe4596d01b8d8f5d7f95e3a2af544d031c58
thevpc <taha.bensalah@gmail.com>
2025-10-19 19:53:26 +0100

    Update MainFrame.java :

--- app/ntexup/src/main/java/net/thevpc/ntexup/main/MainFrame.java
+++ app/ntexup/src/main/java/net/thevpc/ntexup/main/MainFrame.java
@@ -5,19 +5,24 @@ import net.thevpc.ntexup.engine.impl.DefaultNTxEngine;
 import net.thevpc.ntexup.main.components.EntryComponent;
 import net.thevpc.ntexup.engine.util.NTxUtilsImages;
 import net.thevpc.nuts.io.NPath;
+import net.thevpc.nuts.util.NBlankable;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainFrame extends JFrame {
 
     private NTxServiceHelper serviceHelper;
     private EntryComponent entryComponent;
+    private final List<ProgressItem> progressIems = new ArrayList<>();
+    private final JProgressBar progressBar = new JProgressBar();
 
     public MainFrame(NTxEngine engine) {
-        serviceHelper = new NTxServiceHelper(this,engine==null?new DefaultNTxEngine() :  engine);
+        serviceHelper = new NTxServiceHelper(this, engine == null ? new DefaultNTxEngine() : engine);
         setTitle("Ntexup Viewer");
         this.setIconImage(
                 NTxUtilsImages.resizeImage(
@@ -46,6 +51,7 @@ public class MainFrame extends JFrame {
         jPanel.add(createMenu(), BorderLayout.NORTH);
         entryComponent = new EntryComponent(serviceHelper);
         jPanel.add(entryComponent, BorderLayout.CENTER);
+        jPanel.add(progressBar, BorderLayout.SOUTH);
         return jPanel;
     }
 
@@ -132,4 +138,76 @@ public class MainFrame extends JFrame {
         return serviceHelper.getEngine();
     }
 
+    public ProgressItem addProgressItem() {
+        ProgressItem e = new MyProgressItem();
+        progressIems.add(e);
+        return e;
+    }
+
+    public interface ProgressItem {
+        void dispose();
+
+        String text();
+    }
+
+    private class MyProgressItem implements ProgressItem {
+        private String text;
+
+        public String text() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+            doUpdateProgressBar();
+        }
+
+        @Override
+        public void dispose() {
+            synchronized (progressIems) {
+                progressIems.remove(this);
+                doUpdateProgressBar();
+            }
+        }
+    }
+
+    private static class ProgressInfo {
+        private String text;
+        private boolean indeterminate;
+        private int value;
+
+        public ProgressInfo(String text, boolean indeterminate, int value) {
+            this.text = text;
+            this.indeterminate = indeterminate;
+            this.value = value;
+        }
+    }
+
+    private ProgressInfo resolveProgressInfo() {
+        synchronized (progressIems) {
+            if (progressIems.isEmpty()) {
+                return new ProgressInfo("", false, 0);
+            } else {
+                for (int i = progressIems.size() - 1; i >= 0; i--) {
+                    ProgressItem u = progressIems.get(i);
+                    String t = u.text();
+                    if (!NBlankable.isBlank(t)) {
+                        progressBar.setIndeterminate(true);
+                        progressBar.setString(t);
+                        return new ProgressInfo(t, true, 0);
+                    }
+                }
+                return new ProgressInfo("", true, 0);
+            }
+        }
+    }
+
+    private void doUpdateProgressBar() {
+        ProgressInfo i = resolveProgressInfo();
+        SwingUtilities.invokeLater(() -> {
+            progressBar.setIndeterminate(i.indeterminate);
+            progressBar.setString(i.text);
+            progressBar.setValue(i.value);
+        });
+    }
 }
