f07b4ef3f09cbe212b12421023283eb473e13a16
thevpc <taha.bensalah@gmail.com>
2025-11-19 03:34:23 +0100

    Update NScore.java : updated-score

--- companions/nscoreboard/src/main/java/net/thevpc/net/nscoreboard/model/NScore.java
+++ companions/nscoreboard/src/main/java/net/thevpc/net/nscoreboard/model/NScore.java
@@ -5,6 +5,7 @@ public class NScore implements Cloneable {
     public int index;
     public String name;
     public String subName;
+    public String disclosedName;
     public double score;
     public NColorApplier background;
     public NColorApplier foreground;
@@ -13,12 +14,14 @@ public class NScore implements Cloneable {
     public int position;
     public int lastPosition;
     public double positionMove;
+    public boolean finished;
 
-    public NScore(String name, String subName, double score) {
+    public NScore(String name, String subName,String disclosedName, double score) {
         this.index = 0;
         this.name = name;
         this.subName = subName;
         this.score = score;
+        this.disclosedName = disclosedName;
     }
 
     public NScore copy() {
