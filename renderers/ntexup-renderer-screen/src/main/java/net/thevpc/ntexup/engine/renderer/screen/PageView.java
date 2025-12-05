b25dfc41d6afe7ff17e8e4262fb47b6783c282cc
thevpc <taha.bensalah@gmail.com>
2025-12-05 20:23:13 +0100

    [CHANGED] now force screen width to height ratio

--- renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/PageView.java
+++ renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/PageView.java
@@ -26,6 +26,7 @@ public class PageView extends JComponent {
     private NTxEngine engine;
     private NTxCompiledDocument document;
     private final NRef<Dimension> lastSize = NRef.ofNull();
+    private final double ratio = 16.0 / 9.0;
 
     public PageView(
             NTxCompiledDocument document,
@@ -36,7 +37,6 @@ public class PageView extends JComponent {
         this.page = page;
         this.uuid = UUID.randomUUID().toString();
         this.engine = engine;
-
     }
 
     public NTxEngine engine() {
@@ -88,29 +88,70 @@ public class PageView extends JComponent {
                 }
             }
             Graphics2D g2d = (Graphics2D) g;
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-            g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-            g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-            g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-            g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
-            g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-
-            NTxNode p = page.compiledPage();
-            NTxNodeRenderer r = engine.getRenderer(p.type()).get();
-            NTxBounds2 bounds = new NTxBounds2(0, 0, size.getWidth(), size.getHeight());
-            NTxNodeRendererContext ctx = new DefaultNTxNodeRendererContext(p, engine,
-                    engine.createGraphics(g2d)
-                    , bounds, bounds, bounds, page, someChange, pageStartTime, NMaps.of(NTxNodeRendererContext.CAPABILITY_ANIMATE, true), this, this::repaint, null, false);
-            if (someChange) {
-                p.invalidateRenderCache();
-            }
-            r.render(ctx);
+            NRef<NTxNode> pageNode = NRef.ofNull();
+//            if(true) {
+//                int pw = getWidth();
+//                int ph = getHeight();
+//
+//                // 1. compute inner ratio rectangle
+//                int w = pw;
+//                int h = (int) (w / ratio);
+//
+//                if (h > ph) {
+//                    h = ph;
+//                    w = (int) (h * ratio);
+//                }
+//
+//                int x = (pw - w) / 2;
+//                int y = (ph - h) / 2;
+//                // 2. paint boundary
+//                g2d.setColor(Color.DARK_GRAY);  // your letterbox color
+//                g2d.fillRect(0, 0, pw, ph);
+//
+//                // 3. clip to inner rect (optional but cleaner)
+//                Shape oldClip = g2d.getClip();
+//                g2d.setClip(x, y, w, h);
+//
+//                // 4. scale your graphics to fit the inner rectangle
+//                double scaleX = w / REF_SIZE.getWidth();
+//                double scaleY = h / REF_SIZE.getHeight();
+//                double scale = Math.min(scaleX, scaleY);
+//
+//                g2d.translate(x, y);
+//                g2d.scale(scale, scale);
+//                renderPage(g2d, size.getWidth(), size.getHeight(), someChange, pageNode);
+//                g2d.setClip(oldClip);
+//            }else{
+                renderPage(g2d, size.getWidth(), size.getHeight(), someChange, pageNode);
+//            }
             c.stop();
             if (someChange) {
-                engine().log().log(NMsg.ofC("paintComponent %s", c, NTxUtils.sourceOf(p)));
+                engine().log().log(NMsg.ofC("paintComponent %s", c, NTxUtils.sourceOf(pageNode.get())));
             }
         }
+    }
+
+    private void renderPage(Graphics2D g2d, double width, double height, boolean someChange,NRef<NTxNode> pageNode) {
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        NTxNode p = page.compiledPage();
+        pageNode.set(p);
+        NTxNodeRenderer r = engine.getRenderer(p.type()).get();
+
+
+        NTxBounds2 bounds = new NTxBounds2(0, 0, width, height);
+        NTxNodeRendererContext ctx = new DefaultNTxNodeRendererContext(p, engine,
+                engine.createGraphics(g2d)
+                , bounds, bounds, bounds, page, someChange, pageStartTime, NMaps.of(NTxNodeRendererContext.CAPABILITY_ANIMATE, true), this, this::repaint, null, false);
+        if (someChange) {
+            p.invalidateRenderCache();
+        }
+        r.render(ctx);
     }
 
 
