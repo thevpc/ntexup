e391dc8f73014b329f7a9de90e504745ac8509a6
thevpc <taha.bensalah@gmail.com>
2025-12-11 19:37:22 +0100

    Update EditorActionOptions.java : prepare-integration

--- app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/options/EditorActionOptions.java
+++ app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/options/EditorActionOptions.java
@@ -1,76 +1,16 @@
 package net.thevpc.ntexup.cmdline.options;
 
-import net.thevpc.nuts.io.NPath;
-import net.thevpc.nuts.util.NStringUtils;
-
-import java.util.ArrayList;
-import java.util.List;
+import net.thevpc.ntexup.cmdline.NEditorSyntaxInstaller;
 
 public class EditorActionOptions extends ActionOptions {
-    public boolean idea;
-    public boolean kate;
-    public boolean gedit;
-    public boolean jedit;
-    public boolean vim;
-    public boolean vscode;
-    public boolean nodepadpp;
-    public boolean force;
+    private NEditorSyntaxInstaller.Info syntaxInfo=new NEditorSyntaxInstaller.Info();
 
     public EditorActionOptions() {
         super(Action.EDITOR);
     }
 
-    public void withEditor(String name) {
-        for (String s : NStringUtils.split(NStringUtils.trim(name).toLowerCase(), ",;:|")) {
-            switch (s) {
-                case "idea":
-                case "intellij": {
-                    idea = true;
-                    break;
-                }
-                case "kate": {
-                    kate = true;
-                    break;
-                }
-                case "gedit": {
-                    gedit = true;
-                    break;
-                }
-                case "vi":
-                case "vim": {
-                    vim = true;
-                    break;
-                }
-                case "jedit": {
-                    jedit = true;
-                    break;
-                }
-                case "nodepad++":
-                case "nodepadpp":
-                {
-                    nodepadpp = true;
-                    break;
-                }
-                case "vscode":
-                {
-                    vscode = true;
-                    break;
-                }
-                case "all": {
-                    vim = true;
-                    idea = true;
-                    gedit = true;
-                    kate = true;
-                    jedit = true;
-                    nodepadpp = true;
-                    vscode = true;
-                    break;
-                }
-                default: {
-                    throw new IllegalArgumentException("unknown editor: " + name);
-                }
-            }
-        }
-    }
 
+    public NEditorSyntaxInstaller.Info getSyntaxInfo() {
+        return syntaxInfo;
+    }
 }
