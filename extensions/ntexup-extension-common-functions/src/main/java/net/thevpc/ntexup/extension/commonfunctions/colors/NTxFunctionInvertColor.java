c043e6ba745afef3c8f27f8b8f53c61b29a47c3e
thevpc <taha.bensalah@gmail.com>
2025-12-19 09:03:06 +0100

    Update NTxFunctionInvertColor.java : fixing-3d

---
+++ extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionInvertColor.java
@@ -0,0 +1,29 @@
+package net.thevpc.ntexup.extension.commonfunctions.colors;
+
+import net.thevpc.ntexup.api.eval.*;
+import net.thevpc.ntexup.api.extension.NTxFunction;
+import net.thevpc.ntexup.api.util.NTxElementUtils;
+import net.thevpc.ntexup.extension.commonfunctions.util.NTxColorUtils;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.text.NMsg;
+
+import java.awt.*;
+
+public class NTxFunctionInvertColor implements NTxFunction {
+    @Override
+    public String name() {
+        return "invertColor";
+    }
+
+    @Override
+    public NElement invoke(NTxFunctionArgs args, NTxFunctionContext context) {
+        if (args.size() == 0) {
+            return NElement.ofNull();
+        }
+        if (args.size() > 1) {
+            context.log().log(NMsg.ofC("%s: expected 1 argument, got %s",name(), args.size()));
+        }
+        Color c = NTxValue.of(args.eval(0)).asColor().get();
+        return NTxElementUtils.toElement(NTxColorUtils.invert(c));
+    }
+}
