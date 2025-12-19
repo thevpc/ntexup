6559d649b7fdddf358fb24477676870cee3f417f
thevpc <taha.bensalah@gmail.com>
2025-12-19 09:03:06 +0100

    Update NTxPentagonBuilder.java : fixing-3d

---
+++ extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/shape/NTxPentagonBuilder.java
@@ -0,0 +1,23 @@
+package net.thevpc.ntexup.extension.shapes2d.shape;
+
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.style.NTxProperties;
+import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
+import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
+import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+
+public class NTxPentagonBuilder implements NTxNodeBuilder {
+
+    NTxProperties defaultStyles = new NTxProperties();
+
+    @Override
+    public void build(NTxNodeBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.PENTAGON)
+                .renderComponent(this::render);
+    }
+
+    public void render(NTxNodeRendererContext rendererContext, NTxNodeBuilderContext buildContext) {
+        NTxPolygonHelper.renderPointsCount(5, rendererContext, defaultStyles);
+    }
+
+}
