af984eae3ba095521deccd28b5d886446e48aead
thevpc <taha.bensalah@gmail.com>
2025-12-19 09:03:04 +0100

    Update AbstractNTxElement3DPrimitive.java : fixing-3d

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem3d/AbstractNTxElement3DPrimitive.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem3d/AbstractNTxElement3DPrimitive.java
@@ -3,45 +3,6 @@ package net.thevpc.ntexup.api.document.elem3d;
 import java.awt.*;
 
 public abstract class AbstractNTxElement3DPrimitive extends AbstractNTxElement3D implements NtxElement3DPrimitive {
-    private Paint backgroundPaint;
-    private Paint contourPaint;
-    private Stroke contourStroke;
-    private Composite composite;
 
 
-    public Composite getComposite() {
-        return composite;
-    }
-
-    public NtxElement3DPrimitive setComposite(Composite composite) {
-        this.composite = composite;
-        return this;
-    }
-
-    public Paint getBackgroundPaint() {
-        return backgroundPaint;
-    }
-
-    public NtxElement3DPrimitive setBackgroundPaint(Paint backgroundPaint) {
-        this.backgroundPaint = backgroundPaint;
-        return this;
-    }
-
-    public Paint getLinePaint() {
-        return contourPaint;
-    }
-
-    public NtxElement3DPrimitive setLinePaint(Paint contourPaint) {
-        this.contourPaint = contourPaint;
-        return this;
-    }
-
-    public Stroke getLineStroke() {
-        return contourStroke;
-    }
-
-    public NtxElement3DPrimitive setLineStroke(Stroke contourStroke) {
-        this.contourStroke = contourStroke;
-        return this;
-    }
 }
