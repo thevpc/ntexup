45b538c2198d428a9aaa60589e0cfa9f164eb552
thevpc <taha.bensalah@gmail.com>
2025-12-19 09:03:04 +0100

    Update NtxElement3DPrimitive.java : fixing-3d

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem3d/NtxElement3DPrimitive.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem3d/NtxElement3DPrimitive.java
@@ -1,23 +1,7 @@
 package net.thevpc.ntexup.api.document.elem3d;
 
-import java.awt.*;
-
 public interface NtxElement3DPrimitive extends NtxElement3D {
     NTxElement3DPrimitiveType type();
+    NTxPoint3D[] points();
 
-    Paint getBackgroundPaint();
-
-    NtxElement3DPrimitive setBackgroundPaint(Paint backgroundPaint);
-
-    Paint getLinePaint();
-
-    NtxElement3DPrimitive setLinePaint(Paint contourPaint);
-
-    Stroke getLineStroke();
-
-    NtxElement3DPrimitive setLineStroke(Stroke contourStroke);
-
-    Composite getComposite();
-
-    NtxElement3DPrimitive setComposite(Composite composite);
 }
