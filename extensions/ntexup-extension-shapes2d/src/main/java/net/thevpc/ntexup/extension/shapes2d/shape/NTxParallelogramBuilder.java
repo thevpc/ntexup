39b135a6ce912e292339353b0ebc932e6c2b1b09
thevpc <taha.bensalah@gmail.com>
2025-12-19 09:03:06 +0100

    Update NTxParallelogramBuilder.java : fixing-3d

---
+++ extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/shape/NTxParallelogramBuilder.java
@@ -0,0 +1,31 @@
+package net.thevpc.ntexup.extension.shapes2d.shape;
+
+import net.thevpc.ntexup.api.document.elem2d.NTxPoint2D;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.style.NTxProperties;
+import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
+import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
+import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+
+public class NTxParallelogramBuilder implements NTxNodeBuilder {
+
+    private NTxProperties defaultStyles = new NTxProperties();
+
+    @Override
+    public void build(NTxNodeBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.PARALLELOGRAM)
+                .renderComponent((rendererContext, builderContext1) -> renderMain(rendererContext, builderContext1));
+    }
+
+    public void renderMain(NTxNodeRendererContext rendererContext, NTxNodeBuilderContext builderContext) {
+        rendererContext = rendererContext.withDefaultStyles(defaultStyles);
+        double w = 20;
+        NTxPoint2D[] points = new NTxPoint2D[]{
+                new NTxPoint2D(0, 100),
+                new NTxPoint2D(100 - w, 100),
+                new NTxPoint2D(100, 0),
+                new NTxPoint2D(w, 0),
+        };
+        NTxPolygonHelper.renderPoints(points, rendererContext);
+    }
+}
