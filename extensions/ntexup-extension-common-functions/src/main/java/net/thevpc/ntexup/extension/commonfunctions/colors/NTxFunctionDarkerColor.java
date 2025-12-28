6692adc78ca264b14d771a6eccdd5e6dd449670b
thevpc <taha.bensalah@gmail.com>
2025-12-28 02:05:28 +0100

    Update NTxFunctionDarkerColor.java : better-ui

--- extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionDarkerColor.java
+++ extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionDarkerColor.java
@@ -27,7 +27,7 @@ public class NTxFunctionDarkerColor implements NTxFunction {
             return NTxElementUtils.toElement(NTxColorUtils.darker(c));
         }
         if (args.size() > 2) {
-            context.log().log(NMsg.ofC("%s: expected 2 arguments, got %s", name(), args.size()));
+            context.log().log(NMsg.ofC("%s: expected 2 arguments, got %s", NMsg.ofStyledKeyword(name()), args.size()));
         }
         float degrees = NTxValue.of(args.eval(1)).asFloat().get();
         return NTxElementUtils.toElement(NTxColorUtils.darker(c, degrees));
