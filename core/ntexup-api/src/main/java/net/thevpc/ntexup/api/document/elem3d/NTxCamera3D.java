a3270b6cf12442198e8115527cd442d0ed460957
thevpc <taha.bensalah@gmail.com>
2026-01-06 19:46:52 +0100

    Update NTxCamera3D.java : wip-3d

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem3d/NTxCamera3D.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem3d/NTxCamera3D.java
@@ -4,8 +4,11 @@ import net.thevpc.ntexup.api.document.elem2d.NTxPoint2D;
 
 public interface NTxCamera3D {
 
+    NTxPoint3D getPosition();
+
     NTxMatrix3D getViewMatrix();
 
-    NTxPoint2D projectFromWorldToScreen(NTxPoint3D worldPoint, NTxPoint2D screenOrigin) ;
-    NTxPoint2D projectFromLocalToScreen(NTxPoint3D localPoint, NTxMatrix3D worldTransform, NTxPoint2D screenOrigin) ;
+    NTxPoint2D[] projectFromWorldToScreen(NTxPoint3D[] worldPoint, NTxPoint2D screenOrigin);
+
+    NTxPoint2D projectFromWorldToScreen(NTxPoint3D worldPoint, NTxPoint2D screenOrigin);
 }
