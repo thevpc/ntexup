b7e4dffc41e64247ef110cf584e89d3451bd295d
thevpc <taha.bensalah@gmail.com>
2026-01-07 17:02:38 +0100

    Update NTxMesh3DTriangleIndices.java : wip-3d

---
+++ extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/api/composite/NTxMesh3DTriangleIndices.java
@@ -0,0 +1,14 @@
+package net.thevpc.ntexup.extension.shapes3d.api.composite;
+
+public class NTxMesh3DTriangleIndices {
+    int p1;
+    int p2;
+    int p3;
+
+    public NTxMesh3DTriangleIndices(int p1, int p2, int p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+}
