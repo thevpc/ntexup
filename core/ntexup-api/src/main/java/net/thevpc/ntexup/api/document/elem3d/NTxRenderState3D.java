e2b39015a1636ce86370826cd0991104e7283362
thevpc <taha.bensalah@gmail.com>
2026-01-07 17:02:36 +0100

    Rename NTxRenderState3D.java → NTxRenderState3D.java : wip-3d

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem3d/NTxRenderState3D.java
+++
@@ -1,7 +0,0 @@
-package net.thevpc.ntexup.api.document.elem3d;
-
-public interface NTxRenderState3D {
-    NTxVector3D lightOrientation();
-
-    NtxElement3DPrimitive[] toPrimitives(NtxElement3D e);
-}
