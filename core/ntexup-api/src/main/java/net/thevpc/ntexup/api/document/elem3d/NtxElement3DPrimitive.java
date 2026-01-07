0c53d3a5e6af73d3cb465fb88d86a2c0c669e2d1
thevpc <taha.bensalah@gmail.com>
2026-01-07 17:02:36 +0100

    Rename NtxElement3DPrimitive.java → NtxElement3DPrimitive.java : wip-3d

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem3d/NtxElement3DPrimitive.java
+++
@@ -1,7 +0,0 @@
-package net.thevpc.ntexup.api.document.elem3d;
-
-public interface NtxElement3DPrimitive extends NtxElement3D {
-    NTxElement3DPrimitiveType type();
-    NTxPoint3D[] points();
-
-}
