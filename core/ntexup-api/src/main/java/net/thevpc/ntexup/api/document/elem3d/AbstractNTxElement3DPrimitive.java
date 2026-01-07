94d21ea965287c29a84949cbd34fc040cb072a73
thevpc <taha.bensalah@gmail.com>
2026-01-07 17:02:36 +0100

    Rename AbstractNTxElement3DPrimitive.java → AbstractNTxElement3DPrimitive.java : wip-3d

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem3d/AbstractNTxElement3DPrimitive.java
+++
@@ -1,8 +0,0 @@
-package net.thevpc.ntexup.api.document.elem3d;
-
-import java.awt.*;
-
-public abstract class AbstractNTxElement3DPrimitive extends AbstractNTxElement3D implements NtxElement3DPrimitive {
-
-
-}
