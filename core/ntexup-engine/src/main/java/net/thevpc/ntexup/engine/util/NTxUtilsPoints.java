71911515a04e96fec2a7b2e219baaf83540bc3c6
thevpc <taha.bensalah@gmail.com>
2026-01-07 17:02:36 +0100

    Update NTxUtilsPoints.java : wip-3d

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/util/NTxUtilsPoints.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/util/NTxUtilsPoints.java
@@ -1,7 +1,6 @@
 package net.thevpc.ntexup.engine.util;
 
 import net.thevpc.ntexup.api.document.elem2d.NTxPoint2D;
-import net.thevpc.ntexup.api.document.elem3d.NTxPoint3D;
 import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.style.NTxPropName;
 import net.thevpc.ntexup.api.eval.NTxValue;
@@ -10,22 +9,12 @@ import net.thevpc.nuts.util.NOptional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class NTxUtilsPoints {
     static
     public boolean addPoints(NTxNode line, NTxPoint2D[] points) {
         boolean result = false;
         for (NTxPoint2D point : points) {
-            result |= addPoint(line, point);
-        }
-        return result;
-    }
-
-    static
-    public boolean addPoints(NTxNode line, NTxPoint3D[] points) {
-        boolean result = false;
-        for (NTxPoint3D point : points) {
             result |= addPoint(line, point);
         }
         return result;
@@ -47,19 +36,4 @@ public class NTxUtilsPoints {
         return false;
     }
 
-    static
-    public boolean addPoint(NTxNode line, NTxPoint3D point) {
-        if (point != null) {
-            NTxValue o = NTxValue.of(line.getPropertyValue(NTxPropName.POINTS).orNull());
-            NOptional<NTxPoint3D[]> hPoint2DArray = o.asPoint3DArray();
-            List<NTxPoint3D> v = new ArrayList<>();
-            if (hPoint2DArray.isPresent()) {
-                v.addAll(Arrays.asList(hPoint2DArray.get()));
-            }
-            v.add(point);
-            line.setProperty(NTxPropName.POINTS, NTxElementUtils.toElement(v.toArray(new NTxPoint3D[0])));
-            return true;
-        }
-        return false;
-    }
 }
