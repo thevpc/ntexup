5686e4d40bbdef05d3488731004daf9feaa96bc6
thevpc <taha.bensalah@gmail.com>
2026-01-07 17:02:38 +0100

    Update NtxGraphics3D.java : wip-3d

---
+++ extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/api/NtxGraphics3D.java
@@ -0,0 +1,14 @@
+package net.thevpc.ntexup.extension.shapes3d.api;
+
+import net.thevpc.ntexup.api.document.elem2d.NTxPoint2D;
+
+public interface NtxGraphics3D {
+    void transform3D(NTxMatrix3D transform3D);
+
+    void draw3D(NtxElement3D element3D, NTxPoint2D origin);
+
+    NTxLight3D getLight3D();
+
+    NTxLight3D setLight3D(NTxLight3D light3D);
+
+}
