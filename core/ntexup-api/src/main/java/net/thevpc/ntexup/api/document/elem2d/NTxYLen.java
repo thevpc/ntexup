2a1933be35475873891af312cc7cc812138a36b3
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:41 +0100

    Update NTxYLen.java : better-tson

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem2d/NTxYLen.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem2d/NTxYLen.java
@@ -37,7 +37,7 @@ public class NTxYLen {
         this.root = root;
     }
 
-    public double value(NTxBounds2 parentBounds, NTxBounds2 screenBounds) {
+    public double value(NTxBounds2D parentBounds, NTxBounds2D screenBounds) {
         if (this.root) {
             if (value >= 100) {
                 return screenBounds.getHeight();
