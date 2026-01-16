f7cb4c4d083ed294b39fa26faff1360137c6bd1e
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:45 +0100

    Update NtxFace.java : better-tson

---
+++ lib/ntexup-lib-geometry3d/src/main/java/net/thevpc/ntexup/lib/geometry3d/NtxFace.java
@@ -0,0 +1,15 @@
+package net.thevpc.ntexup.lib.geometry3d;
+
+import java.awt.*;
+
+public interface NtxFace {
+    boolean isDrawContour();
+
+    boolean isVisible();
+
+    Paint getBackground();
+
+    Stroke getStroke();
+
+    boolean isFillBackground();
+}
