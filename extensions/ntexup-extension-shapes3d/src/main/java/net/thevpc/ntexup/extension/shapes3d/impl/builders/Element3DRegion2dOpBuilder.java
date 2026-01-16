87200fabd4e1d91687a8d9b7d08355be57b40279
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:43 +0100

    Update Element3DRegion2dOpBuilder.java : better-tson

---
+++ extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/impl/builders/Element3DRegion2dOpBuilder.java
@@ -0,0 +1,42 @@
+package net.thevpc.ntexup.extension.shapes3d.impl.builders;
+
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2D;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.style.NTxPropName;
+import net.thevpc.ntexup.api.eval.NTxValue;
+import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+import net.thevpc.ntexup.extension.shapes3d.impl.NtxElement3DNodeParser;
+import net.thevpc.ntexup.extension.shapes3d.impl.NtxShapes3dUtils;
+import net.thevpc.ntexup.extension.shapes3d.impl.RealToRelativeMapper;
+import net.thevpc.ntexup.lib.geometry2d.NTxElement2DFactory;
+import net.thevpc.ntexup.lib.geometry2d.NTxRegion2D;
+import net.thevpc.ntexup.lib.geometry3d.NTxPoint3D;
+import net.thevpc.ntexup.lib.geometry3d.NtxElement3D;
+import net.thevpc.ntexup.lib.geometry3d.impl.composite.NtxElement3DPrism;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Element3DRegion2dOpBuilder implements NtxElement3DNodeParser {
+    @Override
+    public List<String> getId3d() {
+        return Arrays.asList("substruct","concat","union","intersect","reverse-intersect");
+    }
+
+    @Override
+    public NtxElement3D createElement3D(NTxNode node, NTxNodeRendererContext rendererContext, NTxBounds2D b, RealToRelativeMapper mapper, NtxElement3DNodeParserFactory parserFactory) {
+        NTxRegion2D r = NTxElement2DFactory.region(NtxShapes3dUtils.nodeToElement(node)).orNull();
+        if (r != null) {
+            NTxPoint3D position = NtxShapes3dUtils.resolvePoint(node, NTxPropName.POSITION, "real-position", NTxPoint3D::ofZero, b, mapper);
+            double thickness = NTxValue.ofProp(node, "thickness").asDouble().orElse(0.0);
+            return new NtxElement3DPrism(
+                    position,
+                    r,
+                    thickness
+            );
+        }
+        return null;
+    }
+
+
+}
