090e2396291426790323116b8fe27e4076112772
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:41 +0100

    Update NTxNodeRenderer.java : better-tson

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxNodeRenderer.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxNodeRenderer.java
@@ -1,14 +1,14 @@
 package net.thevpc.ntexup.api.renderer;
 
 import net.thevpc.ntexup.api.document.NTxSizeRequirements;
-import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2D;
 
 
 public interface NTxNodeRenderer {
 
     NTxSizeRequirements sizeRequirements(NTxNodeRendererContext ctx);
 
-    NTxBounds2 selfBounds(NTxNodeRendererContext ctx);
+    NTxBounds2D selfBounds(NTxNodeRendererContext ctx);
 
     void render(NTxNodeRendererContext rendererContext);
 
