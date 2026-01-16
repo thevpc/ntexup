7d3952f56e1641def8c35e5bf694d38695758a02
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:44 +0100

    Update NTxEmptyRegion2DImpl.java : better-tson

---
+++ lib/ntexup-lib-geometry2d/src/main/java/net/thevpc/ntexup/lib/geometry2d/impl/NTxEmptyRegion2DImpl.java
@@ -0,0 +1,17 @@
+package net.thevpc.ntexup.lib.geometry2d.impl;
+
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2D;
+import net.thevpc.ntexup.lib.geometry2d.NTxEmptyRegion2D;
+
+
+public class NTxEmptyRegion2DImpl extends NTxRegion2DBase implements NTxEmptyRegion2D {
+    public static final NTxEmptyRegion2D INSTANCE = new NTxEmptyRegion2DImpl();
+
+    public NTxEmptyRegion2DImpl() {
+    }
+
+    @Override
+    public NTxBounds2D getBounds() {
+        return new NTxBounds2D(0.0, 0.0, 0.0, 0.0);
+    }
+}
