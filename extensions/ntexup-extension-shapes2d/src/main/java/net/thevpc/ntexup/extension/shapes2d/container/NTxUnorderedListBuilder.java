1cb1f4ef5ea275f569dc695e83980230af105038
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:42 +0100

    Update NTxUnorderedListBuilder.java : better-tson

--- extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/container/NTxUnorderedListBuilder.java
+++ extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/container/NTxUnorderedListBuilder.java
@@ -4,7 +4,7 @@
  */
 package net.thevpc.ntexup.extension.shapes2d.container;
 
-import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2D;
 import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.document.style.*;
@@ -30,21 +30,21 @@ public class NTxUnorderedListBuilder implements NTxNodeBuilder {
         ;
     }
 
-    public NTxBounds2 selfBounds(NTxNodeRendererContext rendererContext, NTxNodeBuilderContext builderContext) {
+    public NTxBounds2D selfBounds(NTxNodeRendererContext rendererContext) {
         NTxNode node = rendererContext.node();
         rendererContext = rendererContext.withDefaultStyles(defaultStyles);
-        List<NTxListHelper.NodeWithIndent> all = NTxListHelper.build(node, false,rendererContext, builderContext);
-        NTxBounds2 expectedBounds = rendererContext.defaultSelfBounds();
+        List<NTxListHelper.NodeWithIndent> all = NTxListHelper.build(node, false,rendererContext);
+        NTxBounds2D expectedBounds = rendererContext.defaultSelfBounds();
         for (NTxListHelper.NodeWithIndent a : all) {
             expectedBounds.expand(a.rowBounds);
         }
         return expectedBounds;
     }
 
-    public void renderMain(NTxNodeRendererContext rendererContext, NTxNodeBuilderContext builderContext) {
+    public void renderMain(NTxNodeRendererContext rendererContext) {
         rendererContext = rendererContext.withDefaultStyles(defaultStyles);
         //NTxBounds2 expectedBounds = ctx.selfBounds(p);
-        List<NTxListHelper.NodeWithIndent> all = NTxListHelper.build(rendererContext.node(), false, rendererContext, builderContext);
+        List<NTxListHelper.NodeWithIndent> all = NTxListHelper.build(rendererContext.node(), false, rendererContext);
         for (int i = 0; i < all.size(); i++) {
             NTxListHelper.NodeWithIndent a = all.get(i);
             rendererContext.withChild(a.bullet,a.bulletBounds).render();
