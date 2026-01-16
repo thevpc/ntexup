3b27c7db2d1078487fba20707ed325c099df11cf
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:41 +0100

    Update NTxNodeBuilder.java : better-tson

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/extension/NTxNodeBuilder.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/extension/NTxNodeBuilder.java
@@ -1,8 +1,9 @@
 package net.thevpc.ntexup.api.extension;
 
 import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
+import net.thevpc.nuts.spi.NComponent;
 
-public interface NTxNodeBuilder {
+public interface NTxNodeBuilder extends NComponent {
 
     void build(NTxNodeBuilderContext builderContext);
 
