25f736644ac168527e8d02fd88426fb5382889c6
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:45 +0100

    Rename NTxElement3DRenderer.java → NTxElement3DRenderer.java : better-tson

--- extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/api/NTxElement3DRenderer.java
+++
@@ -1,7 +0,0 @@
-package net.thevpc.ntexup.extension.shapes3d.api;
-
-
-public interface NTxElement3DRenderer {
-    Class<? extends NtxElement3D> forType();
-    NtxElement3DPrimitive[] toPrimitives(NtxElement3D e, NTxRenderState3D renderState);
-}
