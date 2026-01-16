66416dbd4d88f7b2b73fc8619a3ef9de19f908be
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:50 +0100

    Update AbstractNTxElement3DPrimitive.java : better-tson

---
+++ lib/ntexup-lib-geometry3d/src/main/java/net/thevpc/ntexup/lib/geometry3d/impl/primitives/AbstractNTxElement3DPrimitive.java
@@ -0,0 +1,8 @@
+package net.thevpc.ntexup.lib.geometry3d.impl.primitives;
+
+import net.thevpc.ntexup.lib.geometry3d.NtxElement3DPrimitive;
+import net.thevpc.ntexup.lib.geometry3d.impl.AbstractNTxElement3D;
+
+public abstract class AbstractNTxElement3DPrimitive extends AbstractNTxElement3D implements NtxElement3DPrimitive {
+
+}
