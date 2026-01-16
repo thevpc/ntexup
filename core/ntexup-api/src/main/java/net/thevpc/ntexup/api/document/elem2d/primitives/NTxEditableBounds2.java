485b4fa6e75e4846046d644a0c1efa87c1a5beec
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:41 +0100

    Update NTxEditableBounds2.java : better-tson

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem2d/primitives/NTxEditableBounds2.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem2d/primitives/NTxEditableBounds2.java
@@ -1,6 +1,6 @@
 package net.thevpc.ntexup.api.document.elem2d.primitives;
 
-import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2D;
 
 public class NTxEditableBounds2 {
     public Double x;
@@ -22,7 +22,7 @@ public class NTxEditableBounds2 {
     public String toString() {
         return "(" + x + ", " + y + ", " + width + ", " + height + ')';
     }
-    public NTxBounds2 toBounds2() {
-        return new NTxBounds2(x, y, width, height);
+    public NTxBounds2D toBounds2() {
+        return new NTxBounds2D(x, y, width, height);
     }
 }
