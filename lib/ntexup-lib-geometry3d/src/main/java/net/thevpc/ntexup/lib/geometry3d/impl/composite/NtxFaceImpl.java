06bbc9a704268a933316af65a0c5176bee7e1962
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:45 +0100

    Update NtxFaceImpl.java : better-tson

---
+++ lib/ntexup-lib-geometry3d/src/main/java/net/thevpc/ntexup/lib/geometry3d/impl/composite/NtxFaceImpl.java
@@ -0,0 +1,63 @@
+package net.thevpc.ntexup.lib.geometry3d.impl.composite;
+
+import net.thevpc.ntexup.lib.geometry3d.NtxFace;
+
+import java.awt.*;
+
+public class NtxFaceImpl implements NtxFace {
+    private boolean visible=true;
+    private boolean drawContour=true;
+    private boolean fillBackground=true;
+    private Paint background;
+    private Stroke stroke;
+
+    @Override
+    public boolean isDrawContour() {
+        return drawContour;
+    }
+
+    public NtxFaceImpl setDrawContour(boolean drawContour) {
+        this.drawContour = drawContour;
+        return this;
+    }
+
+    @Override
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public NtxFace setVisible(boolean visible) {
+        this.visible = visible;
+        return this;
+    }
+
+    @Override
+    public Paint getBackground() {
+        return background;
+    }
+
+    public NtxFace setBackground(Paint background) {
+        this.background = background;
+        return this;
+    }
+
+    @Override
+    public Stroke getStroke() {
+        return stroke;
+    }
+
+    public NtxFace setStroke(Stroke stroke) {
+        this.stroke = stroke;
+        return this;
+    }
+
+    @Override
+    public boolean isFillBackground() {
+        return fillBackground;
+    }
+
+    public NtxFace setFillBackground(boolean fillBackground) {
+        this.fillBackground = fillBackground;
+        return this;
+    }
+}
