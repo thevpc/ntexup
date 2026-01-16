c533b5693baae59ddc9cf87196674a40317d85f2
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:50 +0100

    Update NTxMesh3DTriangleIndices.java : better-tson

---
+++ lib/ntexup-lib-geometry3d/src/main/java/net/thevpc/ntexup/lib/geometry3d/impl/composite/NTxMesh3DTriangleIndices.java
@@ -0,0 +1,14 @@
+package net.thevpc.ntexup.lib.geometry3d.impl.composite;
+
+public class NTxMesh3DTriangleIndices {
+    int p1;
+    int p2;
+    int p3;
+
+    public NTxMesh3DTriangleIndices(int p1, int p2, int p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+}
