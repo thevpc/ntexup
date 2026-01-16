661dd1fa1cb369bba9463919887a41667f2bbe6f
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:50 +0100

    Update NtxElement3DArc.java : better-tson

---
+++ lib/ntexup-lib-geometry3d/src/main/java/net/thevpc/ntexup/lib/geometry3d/impl/primitives/NtxElement3DArc.java
@@ -0,0 +1,44 @@
+package net.thevpc.ntexup.lib.geometry3d.impl.primitives;
+
+import net.thevpc.ntexup.lib.geometry3d.NTxElement3DPrimitiveType;
+import net.thevpc.ntexup.lib.geometry3d.NTxPoint3D;
+
+public class NtxElement3DArc extends AbstractNTxElement3DPrimitive {
+    private NTxPoint3D from;
+    private NTxPoint3D to;
+    private double startAngle;
+    private double endAngle;
+
+    public NtxElement3DArc(NTxPoint3D from, NTxPoint3D to, double startAngle, double endAngle) {
+        this.from = from;
+        this.to = to;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+    }
+
+    public double getStartAngle() {
+        return startAngle;
+    }
+
+    public double getEndAngle() {
+        return endAngle;
+    }
+
+    public NTxPoint3D getFrom() {
+        return from;
+    }
+
+    public NTxPoint3D getTo() {
+        return to;
+    }
+
+    @Override
+    public NTxElement3DPrimitiveType type() {
+        return NTxElement3DPrimitiveType.ARC;
+    }
+
+    @Override
+    public NTxPoint3D[] points() {
+        return new NTxPoint3D[]{from,to};
+    }
+}
