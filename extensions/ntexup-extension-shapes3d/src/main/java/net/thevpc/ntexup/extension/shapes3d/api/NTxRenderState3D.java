3a75e9ae8fbc58e553f49c4db3ba26ec7a7c4c69
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:45 +0100

    Rename NTxRenderState3D.java → NTxRenderState3D.java : better-tson

--- extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/api/NTxRenderState3D.java
+++
@@ -1,7 +0,0 @@
-package net.thevpc.ntexup.extension.shapes3d.api;
-
-public interface NTxRenderState3D {
-    NTxVector3D lightOrientation();
-
-    NtxElement3DPrimitive[] toPrimitives(NtxElement3D e);
-}
