74e8f4679c51f8448b0252c2af002abfabfefcd0
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:03 +0100

    Update NTx3DMesh.java : fix-fragments

--- lib/ntexup-lib-geometry3d/src/main/java/net/thevpc/ntexup/lib/geometry3d/NTx3DMesh.java
+++ lib/ntexup-lib-geometry3d/src/main/java/net/thevpc/ntexup/lib/geometry3d/NTx3DMesh.java
@@ -1,7 +1,7 @@
 package net.thevpc.ntexup.lib.geometry3d;
 
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+import net.thevpc.ntexup.api.renderer.NTxRendererContext;
 import net.thevpc.ntexup.lib.geometry3d.impl.primitives.NtxElement3DPolygon;
 import net.thevpc.ntexup.lib.geometry3d.impl.primitives.NtxElement3DTriangle;
 
@@ -12,7 +12,7 @@ public interface NTx3DMesh {
 
     boolean refineTriangle(NtxElement3DTriangle triangle, List<? super NtxElement3DTriangle> out);
 
-    NTx3DMesh configureScene(NTxNode polygon, NTxNodeRendererContext rendererContext);
+    NTx3DMesh configureScene(NTxNode polygon, NTxRendererContext rendererContext);
 
     NTx3DMesh configureElement(NtxElement3D polygon);
 
