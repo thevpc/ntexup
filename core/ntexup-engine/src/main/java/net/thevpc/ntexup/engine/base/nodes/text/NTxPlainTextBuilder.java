049d5fd622ee9b8a0baff9a14e3238ba7ac2b59e
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:00 +0100

    Update NTxPlainTextBuilder.java : fix-fragments

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NTxPlainTextBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NTxPlainTextBuilder.java
@@ -6,11 +6,10 @@ package net.thevpc.ntexup.engine.base.nodes.text;
 
 import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
 import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
-import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.document.style.NTxPropName;
 import net.thevpc.ntexup.api.renderer.NTxGraphics;
-import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+import net.thevpc.ntexup.api.renderer.NTxRendererContext;
 import net.thevpc.ntexup.api.renderer.text.NTxRichTextToken;
 import net.thevpc.ntexup.api.renderer.text.NTxRichTextTokenType;
 import net.thevpc.ntexup.api.renderer.text.NTxTextOptions;
@@ -33,16 +32,16 @@ public class NTxPlainTextBuilder implements NTxNodeBuilder {
         ;
     }
 
-    private void renderTextBuildText(String text, NTxTextOptions options, NTxNode p, NTxNodeRendererContext rendererContext, NTxTextRendererBuilder builder) {
+    private void renderTextBuildText(String text, NTxTextOptions options, NTxRendererContext ctx, NTxTextRendererBuilder builder) {
 //        Paint fg = rendererContext.getForegroundColor(p,true);
-        NElement d = p.getPropertyValue(NTxPropName.VALUE).orElse(NElement.ofString(""));
+        NElement d = ctx.computePropertyValue(NTxPropName.VALUE).orElse(NElement.ofString(""));
 
         String message = NStringUtils.trim(d.asStringValue().get());
         String[] allLines = message.trim().split("[\n]");
         for (int i = 0; i < allLines.length; i++) {
             allLines[i] = allLines[i].trim();
             NTxRichTextToken c = new NTxRichTextToken(NTxRichTextTokenType.PLAIN, allLines[i]);
-            NTxGraphics g = rendererContext.graphics();
+            NTxGraphics g = ctx.graphics();
             g.setFont(c.textOptions.baseFont);
             c.bounds = g.getStringBounds(c.text);
             builder.nextLine().addToken(c);
