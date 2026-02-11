40f1f02119948e1b846fcd5869dd9446070ec605
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:00 +0100

    Update NTxFunctionArgsImpl.java : fix-fragments

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/eval/NTxFunctionArgsImpl.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/eval/NTxFunctionArgsImpl.java
@@ -1,10 +1,9 @@
 package net.thevpc.ntexup.engine.eval;
 
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.engine.NTxEngine;
+import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 import net.thevpc.ntexup.api.eval.NTxFunctionArg;
 import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
-import net.thevpc.ntexup.api.eval.NTxVarProvider;
 import net.thevpc.nuts.elem.NElement;
 
 import java.util.Arrays;
@@ -13,11 +12,11 @@ import java.util.List;
 public class NTxFunctionArgsImpl implements NTxFunctionArgs {
     private NTxFunctionArg[] args;
 
-    public NTxFunctionArgsImpl(NElement[] callArgs, NTxNode node, NTxEngine engine, NTxVarProvider varProvider) {
-        this(Arrays.stream(callArgs).map(x -> new NTxFunctionArgImpl(x, node,engine,varProvider)).toArray(NTxFunctionArg[]::new));
+    public NTxFunctionArgsImpl(NElement[] callArgs, NTxResolutionContext context) {
+        this(Arrays.stream(callArgs).map(x -> new NTxFunctionArgImpl(x, context)).toArray(NTxFunctionArg[]::new));
     }
-    public NTxFunctionArgsImpl(List<NElement> callArgs, NTxNode node, NTxEngine engine, NTxVarProvider varProvider) {
-        this(callArgs.stream().map(x -> new NTxFunctionArgImpl(x, node,engine,varProvider)).toArray(NTxFunctionArg[]::new));
+    public NTxFunctionArgsImpl(List<NElement> callArgs, NTxNode node, NTxResolutionContext context) {
+        this(callArgs.stream().map(x -> new NTxFunctionArgImpl(x, context)).toArray(NTxFunctionArg[]::new));
     }
 
     public NTxFunctionArgsImpl(NTxFunctionArg[] args) {
