a086a4a0ac6a764a1103ab6691fdad9f02612963
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:33:59 +0100

    Update NTxFunction.java : fix-fragments

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/extension/NTxFunction.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/extension/NTxFunction.java
@@ -1,11 +1,11 @@
 package net.thevpc.ntexup.api.extension;
 
+import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
-import net.thevpc.ntexup.api.eval.NTxFunctionContext;
 import net.thevpc.nuts.elem.NElement;
 
 public interface NTxFunction {
     String name();
 
-    NElement invoke(NTxFunctionArgs args, NTxFunctionContext context);
+    NElement invoke(NTxFunctionArgs args, NTxResolutionContext context);
 }
