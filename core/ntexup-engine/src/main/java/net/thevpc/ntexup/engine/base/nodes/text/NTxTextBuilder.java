9e4ac092ce3363091d124b117de200188a06fdcc
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:00 +0100

    Update NTxTextBuilder.java : fix-fragments

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NTxTextBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NTxTextBuilder.java
@@ -6,7 +6,6 @@ package net.thevpc.ntexup.engine.base.nodes.text;
 
 import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
 import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
-import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.document.style.NTxPropName;
 import net.thevpc.ntexup.api.renderer.*;
@@ -33,27 +32,27 @@ public class NTxTextBuilder implements NTxNodeBuilder {
         ;
     }
 
-    public void buildText(String text, NTxTextOptions options, NTxNode p, NTxNodeRendererContext ctx, NTxTextRendererBuilder builder) {
+    public void buildText(String text, NTxTextOptions options, NTxRendererContext ctx, NTxTextRendererBuilder builder) {
         NTxTextTokenParseHelper aa = new NTxTextTokenParseHelper(ctx, new NReservedSimpleCharQueue(ctx.engine().tools().trimBloc(text).toCharArray()), ctx.buildContext());
         List<NTxTextToken> all = aa.parse();
         for (NTxTextToken a : all) {
-            consumeSpecialTokenType(a, p, ctx, builder);
+            consumeSpecialTokenType(a, ctx, builder);
         }
     }
 
-    private void consumeSpecialTokenType(NTxTextToken a, NTxNode node, NTxNodeRendererContext ctx, NTxTextRendererBuilder builder) {
+    private void consumeSpecialTokenType(NTxTextToken a, NTxRendererContext ctx, NTxTextRendererBuilder builder) {
         if (a == null) {
             return;
         }
         if (a instanceof NTxTextTokenFlavored) {
             NTxTextTokenFlavored b = (NTxTextTokenFlavored) a;
             // interpolation is the to be done by the flavor implementation
-            builder.appendCustom(b.flavor(), b.value(), b.options(), node, ctx);
+            builder.appendCustom(b.flavor(), b.value(), b.options(), ctx);
         } else {
             NTxTextTokenText b = (NTxTextTokenText) a;
             // apply interpolation
-            String txt2 = ctx.evalExpression(NElement.ofString(b.value()), node).flatMap(NElement::asStringValue).get();
-            builder.appendText(txt2, b.options(), node, ctx);
+            String txt2 = ctx.evalExpression(NElement.ofString(b.value())).flatMap(NElement::asStringValue).get();
+            builder.appendText(txt2, b.options(), ctx);
         }
     }
 
