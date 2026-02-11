6d4aa1d545839e0720e530845f8c1635bcc19100
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:03 +0100

    Update Element3DPolylineBuilder.java : fix-fragments

--- extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/impl/builders/Element3DPolylineBuilder.java
+++ extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/impl/builders/Element3DPolylineBuilder.java
@@ -4,20 +4,16 @@ import net.thevpc.ntexup.api.document.elem2d.NTxBounds2D;
 import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.document.style.NTxPropName;
-import net.thevpc.ntexup.api.eval.NTxValue;
-import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+import net.thevpc.ntexup.api.renderer.NTxRendererContext;
 import net.thevpc.ntexup.extension.shapes3d.impl.NtxElement3DNodeParser;
 import net.thevpc.ntexup.extension.shapes3d.impl.NtxShapes3dUtils;
 import net.thevpc.ntexup.extension.shapes3d.impl.RealToRelativeMapper;
 import net.thevpc.ntexup.lib.geometry3d.NTxPoint3D;
 import net.thevpc.ntexup.lib.geometry3d.NtxElement3D;
-import net.thevpc.ntexup.lib.geometry3d.impl.NTx3DUtils;
 import net.thevpc.ntexup.lib.geometry3d.impl.primitives.NtxElement3DPolyline;
-import net.thevpc.ntexup.lib.geometry3d.impl.primitives.NtxElement3DTriangle;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class Element3DPolylineBuilder implements  NtxElement3DNodeParser {
     @Override
@@ -26,7 +22,7 @@ public class Element3DPolylineBuilder implements  NtxElement3DNodeParser {
     }
 
     @Override
-    public NtxElement3D createElement3D(NTxNode node, NTxNodeRendererContext rendererContext, NTxBounds2D b, RealToRelativeMapper mapper, NtxElement3DNodeParserFactory parserFactory) {
+    public NtxElement3D createElement3D(NTxNode node, NTxRendererContext rendererContext, NTxBounds2D b, RealToRelativeMapper mapper, NtxElement3DNodeParserFactory parserFactory) {
         NTxPoint3D[] points = NtxShapes3dUtils.resolvePoints(node, NTxPropName.POINTS, "real-points", () -> new NTxPoint3D[0], b, mapper);
         NtxElement3DPolyline r = new NtxElement3DPolyline(points);
         NtxShapes3dUtils.apply3dProps(node, r, rendererContext, b);
