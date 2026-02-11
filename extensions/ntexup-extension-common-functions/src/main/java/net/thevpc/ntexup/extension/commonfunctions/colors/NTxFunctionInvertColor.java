b3e0e6b00662955370b339b0ad8df9014dee912c
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:01 +0100

    Update NTxFunctionInvertColor.java : fix-fragments

--- extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionInvertColor.java
+++ extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionInvertColor.java
@@ -16,7 +16,7 @@ public class NTxFunctionInvertColor implements NTxFunction {
     }
 
     @Override
-    public NElement invoke(NTxFunctionArgs args, NTxFunctionContext context) {
+    public NElement invoke(NTxFunctionArgs args, NTxResolutionContext context) {
         if (args.size() == 0) {
             return NElement.ofNull();
         }
