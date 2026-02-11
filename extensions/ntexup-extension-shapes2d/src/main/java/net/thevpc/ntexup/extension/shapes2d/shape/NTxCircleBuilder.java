2212bb2fc635cef9d76c664564215c924bd7aefb
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:02 +0100

    Update NTxCircleBuilder.java : fix-fragments

--- extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/shape/NTxCircleBuilder.java
+++ extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/shape/NTxCircleBuilder.java
@@ -7,7 +7,7 @@ import net.thevpc.ntexup.api.document.style.NTxProperties;
 import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
 import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
 import net.thevpc.ntexup.api.renderer.NTxGraphics;
-import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+import net.thevpc.ntexup.api.renderer.NTxRendererContext;
 import net.thevpc.ntexup.api.util.NTxUtils;
 
 public class NTxCircleBuilder implements NTxNodeBuilder {
@@ -21,10 +21,10 @@ public class NTxCircleBuilder implements NTxNodeBuilder {
                 ;
     }
 
-    public void renderMain(NTxNodeRendererContext nodeRendererContext) {
+    public void renderMain(NTxRendererContext nodeRendererContext) {
         nodeRendererContext = nodeRendererContext.withDefaultStyles(defaultStyles);
         NTxNode node = nodeRendererContext.node();
-        NTxBounds2D b = nodeRendererContext.selfBounds(node, null, null);
+        NTxBounds2D b = nodeRendererContext.selfBounds(null, null);
         double x = b.getX();
         double y = b.getY();
         NTxGraphics g = nodeRendererContext.graphics();
@@ -34,13 +34,13 @@ public class NTxCircleBuilder implements NTxNodeBuilder {
             int hh = NTxUtils.intOf(b.getHeight());
             ww=Math.min(ww,hh);
             hh=ww;
-            if (someBG = nodeRendererContext.applyBackgroundColor(node)) {
+            if (someBG = nodeRendererContext.applyBackgroundColor()) {
                 g.fillOval((int) x, (int) y, ww, hh);
             }
-            if (nodeRendererContext.applyForeground(node, !someBG)) {
+            if (nodeRendererContext.applyForeground(!someBG)) {
                 int finalWw = ww;
                 int finalHh = hh;
-                nodeRendererContext.withStroke(node,()->{
+                nodeRendererContext.withStroke(()->{
                     g.drawOval((int) x, (int) y, finalWw, finalHh);
                 });
             }
