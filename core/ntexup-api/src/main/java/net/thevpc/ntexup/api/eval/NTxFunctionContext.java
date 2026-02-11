2cc5ce5ba7a669a558ebd2fe666000ecf25fc804
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:33:59 +0100

    Update NTxFunctionContext.java : fix-fragments

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxFunctionContext.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxFunctionContext.java
@@ -1,16 +1,16 @@
-package net.thevpc.ntexup.api.eval;
-
-
-import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.engine.NTxEngine;
-import net.thevpc.ntexup.api.log.NTxLogger;
-import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.util.NOptional;
-
-public interface NTxFunctionContext {
-    NTxLogger log();
-    NTxEngine engine();
-    NTxNode node();
-    NOptional<NElement> eval(NElement expr);
-    NOptional<NTxVar> findVar(String varName);
-}
+//package net.thevpc.ntexup.api.eval;
+//
+//
+//import net.thevpc.ntexup.api.document.node.NTxNode;
+//import net.thevpc.ntexup.api.engine.NTxEngine;
+//import net.thevpc.ntexup.api.log.NTxLogger;
+//import net.thevpc.nuts.elem.NElement;
+//import net.thevpc.nuts.util.NOptional;
+//
+//public interface NTxFunctionContext {
+//    NTxLogger log();
+//    NTxEngine engine();
+//    NTxNode node();
+//    NOptional<NElement> eval(NElement expr);
+//    NOptional<NTxVar> findVar(String varName);
+//}
