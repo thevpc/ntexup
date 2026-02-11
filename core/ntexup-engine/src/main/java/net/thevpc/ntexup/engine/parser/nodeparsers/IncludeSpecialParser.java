554f6666e696b39fdc038b65003c229bb045a120
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:01 +0100

    Update IncludeSpecialParser.java : fix-fragments

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/nodeparsers/IncludeSpecialParser.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/nodeparsers/IncludeSpecialParser.java
@@ -1,9 +1,9 @@
 package net.thevpc.ntexup.engine.parser.nodeparsers;
 
+import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 import net.thevpc.ntexup.engine.parser.NTxNodeParserBase;
 import net.thevpc.ntexup.api.document.node.NTxItem;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
-import net.thevpc.ntexup.api.parser.NTxNodeFactoryParseContext;
 import net.thevpc.ntexup.api.util.NTxUtils;
 import net.thevpc.ntexup.engine.parser.ctrlnodes.CtrlNTxNodeInclude;
 import net.thevpc.nuts.concurrent.NScoredCallable;
@@ -18,7 +18,7 @@ public class IncludeSpecialParser extends NTxNodeParserBase {
 
 
     @Override
-    public NScoredCallable<NTxItem> parseNode(NTxNodeFactoryParseContext context) {
+    public NScoredCallable<NTxItem> parseNode(NTxResolutionContext context) {
         NElement tsonElement = context.element();
         switch (tsonElement.type()) {
             case NAMED_UPLET: {
