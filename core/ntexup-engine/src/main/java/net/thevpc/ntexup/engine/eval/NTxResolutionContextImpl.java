79c5dfaed17b60b6def776191d6b791436b80ef4
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:00 +0100

    Update NTxResolutionContextImpl.java : fix-fragments

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/eval/NTxResolutionContextImpl.java
@@ -0,0 +1,437 @@
+package net.thevpc.ntexup.engine.eval;
+
+import net.thevpc.ntexup.api.document.NTxDocument;
+import net.thevpc.ntexup.api.document.NTxDocumentFactory;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NTxNodeDef;
+import net.thevpc.ntexup.api.engine.NTxEngine;
+import net.thevpc.ntexup.api.eval.NTxResolutionContext;
+import net.thevpc.ntexup.api.eval.NTxVar;
+import net.thevpc.ntexup.api.extension.NTxFunction;
+import net.thevpc.ntexup.api.log.NTxLogger;
+import net.thevpc.ntexup.api.source.NTxSource;
+import net.thevpc.ntexup.api.util.NTxElementUtils;
+import net.thevpc.ntexup.api.util.NTxUtils;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.io.NPath;
+import net.thevpc.nuts.text.NMsg;
+import net.thevpc.nuts.util.*;
+
+import java.util.*;
+
+public class NTxResolutionContextImpl implements NTxResolutionContext {
+    protected NElement element;
+    protected NTxNode node;
+    protected NTxNode parent;
+    protected NTxNode[] path;
+    protected NTxNodeDef def;
+    //    NTxNode hierarchy;
+    protected boolean isInPage;
+    protected final NTxEngine engine;
+    protected final NTxDocument document;
+    protected Map<String, NTxVar> vars = new LinkedHashMap<>();
+    protected Map<String, NTxNodeDef> definitions = new LinkedHashMap<>();
+    protected Map<String, NTxFunction> functions = new LinkedHashMap<>();
+
+    public NTxResolutionContextImpl(NTxNode parent, NTxNode node, NElement element, boolean isInPage, NTxEngine engine, NTxDocument document) {
+        this.node = node;
+        this.parent = node;
+        NAssert.requireNamedNonNull(node, "node");
+        if (parent == null) {
+            this.path = new NTxNode[]{node};
+        } else {
+            this.path = new NTxNode[]{parent, node};
+        }
+        this.element = element;
+        this.isInPage = isInPage;
+        this.engine = engine;
+        this.document = document;
+    }
+
+    public NTxResolutionContextImpl(NTxNode[] path, NElement element, NTxNodeDef def, boolean isInPage, NTxEngine engine, NTxDocument document, Map<String, NTxVar> vars, Map<String, NTxNodeDef> definitions, Map<String, NTxFunction> functions) {
+        this.path = Arrays.copyOf(path, path.length);
+        this.node = path[path.length - 1];
+        this.parent = (path.length - 2 >= 0) ? path[path.length - 2] : null;
+        this.def = def;
+        this.element = element;
+        this.isInPage = isInPage;
+        this.engine = engine;
+        this.document = document;
+        this.vars = vars == null ? new LinkedHashMap<>() : new LinkedHashMap<>(vars);
+        this.definitions = definitions == null ? new LinkedHashMap<>() : new LinkedHashMap<>(definitions);
+        this.functions = functions == null ? new LinkedHashMap<>() : new LinkedHashMap<>(functions);
+    }
+
+    protected NTxResolutionContext copyAs(NTxNode[] path, NElement element, NTxNodeDef def, boolean isInPage, NTxEngine engine, NTxDocument document, Map<String, NTxVar> vars, Map<String, NTxNodeDef> definitions, Map<String, NTxFunction> functions) {
+        return new NTxResolutionContextImpl(path, element, def, isInPage, engine, document, vars, definitions, functions);
+    }
+
+    @Override
+    public NTxResolutionContext pushNode(NTxNode node) {
+        NAssert.requireNamedNonNull(node, "node");
+        List<NTxNode> all = new ArrayList<>(Arrays.asList(path));
+        all.add(NAssert.requireNamedNonNull(node, "parent"));
+        setPath(all.toArray(new NTxNode[0]));
+        return this;
+    }
+
+    @Override
+    public NTxResolutionContext popNode() {
+        List<NTxNode> all = new ArrayList<>(Arrays.asList(path));
+        all.remove(all.size() - 1);
+        setPath(all.toArray(new NTxNode[0]));
+        return this;
+    }
+
+    protected void setPath(NTxNode[] path) {
+        this.path = Arrays.copyOf(path, path.length);
+        this.node = path[path.length - 1];
+        this.parent = (path.length - 2 >= 0) ? path[path.length - 2] : null;
+    }
+
+    @Override
+    public NElement element() {
+        return element;
+    }
+
+    @Override
+    public NTxNode parent() {
+        return parent;
+    }
+
+    @Override
+    public NTxNode node() {
+        return node;
+    }
+
+    public NTxNode[] path() {
+        return Arrays.copyOf(path, path.length);
+    }
+
+    @Override
+    public NTxSource source() {
+        return node.source();
+    }
+
+    @Override
+    public NTxNodeDef def() {
+        return def;
+    }
+
+    @Override
+    public boolean inPage() {
+        return isInPage;
+    }
+
+    public NTxDocument document() {
+        return document;
+    }
+
+    public NTxLogger messages() {
+        return engine.log();
+    }
+
+    public NTxEngine engine() {
+        return engine;
+    }
+
+    @Override
+    public NTxResolutionContext copy() {
+        return copyAs(path, element, def, isInPage, engine, document, vars, definitions, functions);
+    }
+
+    @Override
+    public NTxResolutionContext resolveNode(NTxNode next) {
+        return copy().pushNode(next);
+    }
+
+    @Override
+    public NTxResolutionContext setNode(NTxNode node) {
+        NAssert.requireNamedNonNull(node, "node");
+        NTxNode[]  path=Arrays.copyOf(this.path,this.path.length);
+        path[path.length - 1] = node;
+        setPath(path);
+        return this;
+    }
+
+    @Override
+    public NTxResolutionContext setElement(NElement element) {
+        this.element = element;
+        return this;
+    }
+
+    @Override
+    public NTxResolutionContext withNode(NTxNode next) {
+        NTxNode n = NAssert.requireNamedNonNull(next, "parent");
+        return copy().setNode(n);
+    }
+
+    @Override
+    public NTxResolutionContext withElement(NElement element) {
+        if (Objects.equals(element, this.element)) {
+            return this;
+        }
+        return copy().setElement(element);
+    }
+
+    @Override
+    public NTxResolutionContext setInPage(boolean isInPage) {
+        this.isInPage = isInPage;
+        return this;
+    }
+
+    public NTxResolutionContext withInPage(boolean isInPage) {
+        if (this.isInPage == isInPage) {
+            return this;
+        }
+        return copyAs(path, element, def, isInPage, engine, document, vars, definitions, functions);
+    }
+
+    @Override
+    public NTxResolutionContext withParentOnly(NTxNode parent) {
+        if (parent == this.node) {
+            return this;
+        }
+        List<NTxNode> all = new ArrayList<>(Arrays.asList(path));
+        all.add(NAssert.requireNamedNonNull(parent, "parent"));
+        return copyAs(all.toArray(new NTxNode[0]), element, null, isInPage, engine, document, vars, definitions, functions);
+    }
+
+    @Override
+    public NTxResolutionContext withParentOnly() {
+        if (def == null) {
+            return this;
+        }
+        return copyAs(path, element, null, isInPage, engine, document, vars, definitions, functions);
+    }
+
+    @Override
+    public NTxResolutionContext withDef(NTxNodeDef def) {
+        if (def == this.def) {
+            return this;
+        }
+        return copyAs(path, element, def, isInPage, engine, document, vars, definitions, functions);
+    }
+
+    @Override
+    public NTxResolutionContext setVar(String name, NTxVar value) {
+        if (name == null) {
+            return this;
+        }
+        NTxVar old = this.vars.get(name);
+        if (!Objects.equals(value, old)) {
+            if (value == null) {
+                vars.remove(name);
+            } else {
+                vars.put(name, value);
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public NTxResolutionContext withVar(String name, NTxVar value) {
+        if (name == null) {
+            return this;
+        }
+        Object old = this.vars.get(name);
+        if (Objects.equals(value, old)) {
+            return this;
+        }
+        Map<String, NTxVar> vars2 = new LinkedHashMap<>(vars);
+        if (value == null) {
+            vars2.remove(name);
+        } else {
+            vars2.put(name, value);
+        }
+        return copyAs(path, element, def, isInPage, engine, document, vars2, definitions, functions);
+    }
+
+    @Override
+    public NOptional<NElement> getVarValue(String name) {
+        return getVar(name).map(NTxVar::get);
+    }
+
+    public NOptional<NTxVar> getVar(String varName) {
+        NTxVar value = vars.get(varName);
+        if (value != null) {
+            return NOptional.ofNamed(value, varName);
+        }
+        switch (NStringUtils.trim(varName)) {
+            case "HOME": {
+                return NTxVarImpl.ofOptional(varName, () -> NElement.ofString(System.getProperty("user.home")));
+            }
+            case "USERNAME": {
+                return NTxVarImpl.ofOptional(varName, () -> NElement.ofString(System.getProperty("user.name")));
+            }
+        }
+        String v = System.getProperty(varName);
+        if (v != null) {
+            return NTxVarImpl.ofOptional(varName, () -> NElement.ofString(System.getProperty(varName)));
+        }
+        return NOptional.ofNamedEmpty("var " + varName);
+    }
+
+
+    @Override
+    public NTxResolutionContext withRemoveNamedDef(String name) {
+        if (!this.definitions.containsKey(name)) {
+            return this;
+        }
+        Map<String, NTxNodeDef> def2 = new LinkedHashMap<>(definitions);
+        def2.remove(name);
+        return copyAs(path, element, def, isInPage, engine, document, vars, def2, functions);
+    }
+
+    @Override
+    public NTxResolutionContext removeNamedDef(String name) {
+        if (this.definitions.containsKey(name)) {
+            definitions.remove(name);
+        }
+        return this;
+    }
+
+    public NOptional<NTxNodeDef> getNamedDef(String name) {
+        return NOptional.ofNamed(definitions.get(name), name);
+    }
+
+    @Override
+    public NTxResolutionContext setNamedDef(NTxNodeDef value) {
+        if (value == null) {
+            return this;
+        }
+        Object old = this.definitions.get(value.name());
+        if (!Objects.equals(value, old)) {
+            if (value == null) {
+                definitions.remove(value.name());
+            } else {
+                definitions.put(value.name(), value);
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public NTxResolutionContext withNamedDef(NTxNodeDef value) {
+        if (value == null) {
+            return this;
+        }
+        Object old = this.definitions.get(value.name());
+        if (Objects.equals(value, old)) {
+            return this;
+        }
+        Map<String, NTxNodeDef> def2 = new LinkedHashMap<>(definitions);
+        if (value == null) {
+            def2.remove(value.name());
+        } else {
+            def2.put(value.name(), value);
+        }
+        return copyAs(path, element, def, isInPage, engine, document, vars, def2, functions);
+    }
+
+    @Override
+    public NTxResolutionContext withRemoveFunction(String name) {
+        if (!this.functions.containsKey(name)) {
+            return this;
+        }
+        Map<String, NTxFunction> def2 = new LinkedHashMap<>(functions);
+        def2.remove(name);
+        return copyAs(path, element, def, isInPage, engine, document, vars, definitions, def2);
+    }
+
+    @Override
+    public NTxResolutionContext removeFunction(String name) {
+        if (this.functions.containsKey(name)) {
+            functions.remove(name);
+        }
+        return this;
+    }
+
+    @Override
+    public NOptional<NTxFunction> getFunction(String name) {
+        return NOptional.ofNamed(functions.get(name), name);
+    }
+
+    @Override
+    public NTxResolutionContext setFunction(NTxFunction value) {
+        if (value == null) {
+            return this;
+        }
+        Object old = this.functions.get(value.name());
+        if (!Objects.equals(value, old)) {
+            if (value == null) {
+                functions.remove(value.name());
+            } else {
+                functions.put(value.name(), value);
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public NTxResolutionContext withFunction(NTxFunction value) {
+        if (value == null) {
+            return this;
+        }
+        Object old = this.functions.get(value.name());
+        if (Objects.equals(value, old)) {
+            return this;
+        }
+        Map<String, NTxFunction> def2 = new LinkedHashMap<>(functions);
+        if (value == null) {
+            def2.remove(value.name());
+        } else {
+            def2.put(value.name(), value);
+        }
+        return copyAs(path, element, def, isInPage, engine, document, vars, definitions, def2);
+    }
+
+    @Override
+    public boolean isDef() {
+        return def != null;
+    }
+
+    @Override
+    public NOptional<NElement> evalExpression(NElement expression) {
+        if (expression == null) {
+            return NOptional.ofNamedEmpty(String.valueOf(expression));
+        }
+        String baseSrc = NTxUtils.findCompilerDeclarationPath(expression).orNull();
+        NTxNodeEval ne = new NTxNodeEval(this);
+        NElement u = ne.eval(NTxElementUtils.toElement(expression));
+        if (u == null) {
+            u = NElement.ofNull();
+        }
+        if (baseSrc != null) {
+            u = NTxUtils.addCompilerDeclarationPath(u, baseSrc);
+        }
+        return NOptional.ofNamed(u, "expression " + expression);
+    }
+
+    @Override
+    public NTxLogger log() {
+        return engine.log();
+    }
+
+    @Override
+    public NPath resolvePath(NElement path) {
+        if (NBlankable.isBlank(path)) {
+            return null;
+        }
+        if (path.isAnyString()) {
+            String pathStr = path.asStringValue().get();
+            if (NTxGitHelper.isGithubFolder(pathStr)) {
+                return NTxGitHelper.resolveGithubPath(pathStr, log());
+            }
+            NTxSource source = source();
+            return NTxUtils.resolvePath(path, source);
+        }
+        throw new NIllegalArgumentException(NMsg.ofC("unsupported path type", path));
+    }
+
+    @Override
+    public NTxDocumentFactory documentFactory() {
+        return engine().documentFactory();
+    }
+
+}
