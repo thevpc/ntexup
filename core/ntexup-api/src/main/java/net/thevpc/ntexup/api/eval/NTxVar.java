7f15a604676d5f6446879277b3f3f8d2b6d40a66
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:33:59 +0100

    Update NTxVar.java : fix-fragments

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxVar.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxVar.java
@@ -3,5 +3,13 @@ package net.thevpc.ntexup.api.eval;
 import net.thevpc.nuts.elem.NElement;
 
 public interface NTxVar {
+    static NTxVar ofEvaluatedExpression(NElement evaluatedExpr) {
+        return ()->evaluatedExpr==null?NElement.ofNull():evaluatedExpr;
+    }
+
+    static NTxVar ofNonEvaluatedExpression(NElement evaluatedExpr) {
+        return ()->evaluatedExpr==null?NElement.ofNull():evaluatedExpr;
+    }
+
     NElement get();
 }
