11efe2e113df1fefb4430dcc38ad145738ed0308
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:03 +0100

    Update NtxElement3DNodeParser.java : fix-fragments

--- extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/impl/NtxElement3DNodeParser.java
+++ extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/impl/NtxElement3DNodeParser.java
@@ -2,7 +2,7 @@ package net.thevpc.ntexup.extension.shapes3d.impl;
 
 import net.thevpc.ntexup.api.document.elem2d.NTxBounds2D;
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+import net.thevpc.ntexup.api.renderer.NTxRendererContext;
 import net.thevpc.ntexup.extension.shapes3d.impl.builders.NtxElement3DNodeParserFactory;
 import net.thevpc.ntexup.lib.geometry3d.NtxElement3D;
 import net.thevpc.nuts.spi.NComponent;
@@ -11,5 +11,5 @@ import java.util.List;
 
 public interface NtxElement3DNodeParser extends NComponent {
     List<String> getId3d();
-    NtxElement3D createElement3D(NTxNode node, NTxNodeRendererContext rendererContext, NTxBounds2D b, RealToRelativeMapper mapper, NtxElement3DNodeParserFactory parserFactory);
+    NtxElement3D createElement3D(NTxNode node, NTxRendererContext rendererContext, NTxBounds2D b, RealToRelativeMapper mapper, NtxElement3DNodeParserFactory parserFactory);
 }
