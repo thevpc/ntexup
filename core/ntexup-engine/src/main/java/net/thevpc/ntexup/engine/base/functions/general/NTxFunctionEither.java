42008079f36a791f191343ca8e3ca9d6b93da0da
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:00 +0100

    Update NTxFunctionEither.java : fix-fragments

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/general/NTxFunctionEither.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/general/NTxFunctionEither.java
@@ -1,9 +1,9 @@
 package net.thevpc.ntexup.engine.base.functions.general;
 
+import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 import net.thevpc.ntexup.api.extension.NTxFunction;
 import net.thevpc.ntexup.api.eval.NTxFunctionArg;
 import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
-import net.thevpc.ntexup.api.eval.NTxFunctionContext;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NBlankable;
 
@@ -14,7 +14,7 @@ public class NTxFunctionEither implements NTxFunction {
     }
 
     @Override
-    public NElement invoke(NTxFunctionArgs args, NTxFunctionContext context) {
+    public NElement invoke(NTxFunctionArgs args, NTxResolutionContext context) {
         for (NTxFunctionArg arg : args.args()) {
             NElement u = arg.eval();
             if (!NBlankable.isBlank(u)) {
