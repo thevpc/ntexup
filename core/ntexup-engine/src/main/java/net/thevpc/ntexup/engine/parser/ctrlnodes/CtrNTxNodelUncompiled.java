cf9ba89999b8061db73af1e51a2d1d1035905bed
thevpc <taha.bensalah@gmail.com>
2026-02-11 20:34:00 +0100

    Update CtrNTxNodelUncompiled.java : fix-fragments

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/ctrlnodes/CtrNTxNodelUncompiled.java
@@ -0,0 +1,32 @@
+package net.thevpc.ntexup.engine.parser.ctrlnodes;
+
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.source.NTxSource;
+import net.thevpc.nuts.elem.NElement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CtrNTxNodelUncompiled extends CtrlNTxNodeBase {
+
+    public CtrNTxNodelUncompiled(NElement body,NTxSource source) {
+        super(NTxNodeType.CTRL_UNCOMPILED, source);
+        setRaw(body);
+    }
+
+    @Override
+    public NTxNode copy() {
+        CtrNTxNodelUncompiled c = new CtrNTxNodelUncompiled(getRaw(),source());
+        copyTo(c);
+        return c;
+    }
+
+    @Override
+    public String toString() {
+        return "Uncompiled(" +
+                getRaw()
+                + ')';
+    }
+}
