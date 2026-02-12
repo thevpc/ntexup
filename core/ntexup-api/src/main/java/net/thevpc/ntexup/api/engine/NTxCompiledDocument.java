0b49b29a9c6eb42896c9af12e8c090c2ea90f648
thevpc <taha.bensalah@gmail.com>
2026-02-12 22:01:22 +0100

    Update NTxCompiledDocument.java : wip

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxCompiledDocument.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxCompiledDocument.java
@@ -3,6 +3,7 @@ package net.thevpc.ntexup.api.engine;
 import net.thevpc.ntexup.api.document.NTxDocument;
 import net.thevpc.ntexup.api.source.NTxSource;
 
+import java.util.Iterator;
 import java.util.List;
 
 public interface NTxCompiledDocument {
@@ -17,6 +18,8 @@ public interface NTxCompiledDocument {
     String title();
 
     NTxEngine engine();
+
+    Iterator<NTxCompiledPage> pagesIterator();
 
     List<NTxCompiledPage> pages();
 
