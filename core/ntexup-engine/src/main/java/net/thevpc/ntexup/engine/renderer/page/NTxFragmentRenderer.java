c626d8fddd8099c3396195157333ac30a907233d
thevpc <taha.bensalah@gmail.com>
2026-02-12 22:01:22 +0100

    Update NTxFragmentRenderer.java : wip

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/page/NTxFragmentRenderer.java
@@ -0,0 +1,30 @@
+package net.thevpc.ntexup.engine.renderer.page;
+
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2D;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.renderer.NTxGraphics;
+import net.thevpc.ntexup.api.renderer.NTxRendererContext;
+import net.thevpc.ntexup.api.util.NTxSizeRef;
+import net.thevpc.ntexup.engine.renderer.NTxNodeRendererBase;
+import net.thevpc.ntexup.engine.util.NTxNodeRendererUtils;
+import net.thevpc.nuts.elem.NElement;
+
+import java.awt.*;
+
+public class NTxFragmentRenderer extends NTxNodeRendererBase {
+    public NTxFragmentRenderer() {
+        super(NTxNodeType.FRAGMENT);
+    }
+
+    @Override
+    public void renderMain(NTxRendererContext ctx) {
+        NTxBounds2D b = ctx.selfBounds(null, null);
+        for (NTxNode child : ctx.node().children()) {
+            ctx.resolveNode(child, b)
+                    .render();
+        }
+    }
+
+
+}
