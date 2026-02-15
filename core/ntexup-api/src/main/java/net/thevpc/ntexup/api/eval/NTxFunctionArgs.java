2b105c43a6aad3343c647fc16ffb46e94633dfc0
thevpc <taha.bensalah@gmail.com>
2026-02-16 00:49:12 +0100

    Update NTxFunctionArgs.java : fix-perf

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxFunctionArgs.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxFunctionArgs.java
@@ -1,6 +1,10 @@
 package net.thevpc.ntexup.api.eval;
 
 import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NOptional;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public interface NTxFunctionArgs {
     int size();
@@ -8,9 +12,16 @@ public interface NTxFunctionArgs {
     NTxFunctionArg[] args();
 
     NTxFunctionArg arg(int index);
+
     NElement src(int index);
 
     NElement[] eval();
 
     NElement eval(int index);
+
+    <T> T eval(int index, Function<NElement, NOptional<T>> converter, String converterName, Supplier<T> whenError);
+
+    boolean checkTooFewArgs(int minArgs);
+
+    boolean checkTooManyArgs(int maxArgs);
 }
