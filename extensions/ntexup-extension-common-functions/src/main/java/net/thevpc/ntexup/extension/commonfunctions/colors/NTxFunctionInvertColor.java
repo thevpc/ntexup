0bb4715aa69b6e046f4149e4e72074d9fe23d170
thevpc <taha.bensalah@gmail.com>
2026-02-16 00:49:13 +0100

    Update NTxFunctionInvertColor.java : fix-perf

--- extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionInvertColor.java
+++ extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionInvertColor.java
@@ -24,6 +24,9 @@ public class NTxFunctionInvertColor implements NTxFunction {
             context.log().log(NMsg.ofC("%s: expected 1 argument, got %s",NMsg.ofStyledKeyword(name()), args.size()));
         }
         Color c = NTxValue.of(args.eval(0)).asColor().get();
+        if(c==null){
+            return NElement.ofNull();
+        }
         return NTxElementUtils.toElement(NTxColorUtils.invert(c));
     }
 }
