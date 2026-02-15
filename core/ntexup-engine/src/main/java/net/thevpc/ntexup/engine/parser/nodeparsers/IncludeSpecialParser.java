04ebd89d12225e2187e9a01810dd280cd016573b
thevpc <taha.bensalah@gmail.com>
2026-02-16 00:49:13 +0100

    Update IncludeSpecialParser.java : fix-perf

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/nodeparsers/IncludeSpecialParser.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/nodeparsers/IncludeSpecialParser.java
@@ -1,5 +1,6 @@
 package net.thevpc.ntexup.engine.parser.nodeparsers;
 
+import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 import net.thevpc.ntexup.engine.parser.NTxNodeParserBase;
 import net.thevpc.ntexup.api.document.node.NTxItem;
@@ -32,6 +33,11 @@ public class IncludeSpecialParser extends NTxNodeParserBase {
         return NScoredCallable.ofInvalid(NMsg.ofC("missing include from %s", NTxUtils.snippet(tsonElement)).asError());
     }
 
+
+    @Override
+    public void compileNode(NTxNode node, NTxResolutionContext context) {
+
+    }
 
 
 }
