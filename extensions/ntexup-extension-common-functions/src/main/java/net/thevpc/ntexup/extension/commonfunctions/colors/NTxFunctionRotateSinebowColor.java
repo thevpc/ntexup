f361419ba71ded0096bff8618907d89f2c216a24
thevpc <taha.bensalah@gmail.com>
2026-02-16 00:49:13 +0100

    Update NTxFunctionRotateSinebowColor.java : fix-perf

--- extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionRotateSinebowColor.java
+++ extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionRotateSinebowColor.java
@@ -19,17 +19,18 @@ public class NTxFunctionRotateSinebowColor implements NTxFunction {
 
     @Override
     public NElement invoke(NTxFunctionArgs args, NTxResolutionContext context) {
-        if (args.size() == 0) {
+        if (args.checkTooFewArgs(1)) {
+            return NElement.ofNull();
+        }
+        args.checkTooManyArgs(2);
+        Color c = args.eval(0, x -> NTxValue.of(x).asColor(), "color", null);
+        if(c==null){
             return NElement.ofNull();
         }
         if (args.size() == 1) {
-            return args.eval(0);
+            return NTxElementUtils.toElement(c);
         }
-        if (args.size() > 2) {
-            context.log().log(NMsg.ofC("%s: expected 2 arguments, got %s", NMsg.ofStyledKeyword(name()), args.size()));
-        }
-        Color c = NTxValue.of(args.eval(0)).asColor().get();
-        float degrees = NTxValue.of(args.eval(1)).asFloat().get();
+        float degrees = args.eval(1, x -> NTxValue.of(x).asFloat(), "degrees", () -> 0.0f);
         return NTxElementUtils.toElement(NTxColorUtils.rotateSinebowDegrees(c, degrees));
     }
 }
