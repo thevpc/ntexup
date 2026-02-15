648b867bd3ba544aadf096bd85cc4d7739a2b07b
thevpc <taha.bensalah@gmail.com>
2026-02-16 00:49:13 +0100

    Update NTxSpatials2.java : fix-perf

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/util/NTxSpatials2.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.engine.util;
+
+public class NTxSpatials2 {
+    NTxRelativeNumber2 size;
+    NTxRelativeNumber2 position;
+    NTxRelativeNumber2 origin;
+    NTxRelativeNumber2 contentPosition;
+    NTxRelativeNumber2 contentOrigin;
+}
