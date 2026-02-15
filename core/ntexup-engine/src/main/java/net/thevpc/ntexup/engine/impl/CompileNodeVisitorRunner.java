0a8152e15be8fd1f1c8d20049439b5e0d39b033d
thevpc <taha.bensalah@gmail.com>
2026-02-16 00:49:12 +0100

    Update CompileNodeVisitorRunner.java : fix-perf

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/impl/CompileNodeVisitorRunner.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/impl/CompileNodeVisitorRunner.java
@@ -6,6 +6,7 @@ import net.thevpc.ntexup.api.document.style.NTxProp;
 import net.thevpc.ntexup.api.document.style.NTxStyleRule;
 import net.thevpc.ntexup.api.engine.CompileNodeVisitor;
 import net.thevpc.ntexup.api.eval.NTxResolutionContext;
+import net.thevpc.ntexup.api.eval.NTxVar;
 import net.thevpc.ntexup.api.extension.NTxFunction;
 
 class CompileNodeVisitorRunner implements CompileNodeVisitor {
@@ -21,7 +22,7 @@ class CompileNodeVisitorRunner implements CompileNodeVisitor {
 
     @Override
     public void visitDefinition(NTxNodeDef a, NTxResolutionContext context) {
-
+        context.setNamedDef(a);
     }
 
     @Override
@@ -32,5 +33,10 @@ class CompileNodeVisitorRunner implements CompileNodeVisitor {
     @Override
     public void visitProperty(NTxProp a, NTxResolutionContext context) {
 
+    }
+
+    @Override
+    public void visitVar(String varName, NTxVar nTxVar, NTxResolutionContext context) {
+        context.setVar(varName, nTxVar);
     }
 }
