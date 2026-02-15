2c0ac760daacc59fab9e969db2c203d303c067c9
thevpc <taha.bensalah@gmail.com>
2026-02-16 00:49:13 +0100

    Update NTxImportRenderer.java : fix-perf

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/special/NTxImportRenderer.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/special/NTxImportRenderer.java
@@ -4,12 +4,8 @@ import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.renderer.NTxRendererContext;
 import net.thevpc.ntexup.engine.renderer.NTxNodeRendererBase;
 
-public class NTxImportRenderer extends NTxNodeRendererBase {
+public class NTxImportRenderer extends NTxDoNothingRenderer {
     public NTxImportRenderer() {
         super(NTxNodeType.CTRL_IMPORT);
-    }
-
-    @Override
-    public void renderMain(NTxRendererContext ctx) {
     }
 }
