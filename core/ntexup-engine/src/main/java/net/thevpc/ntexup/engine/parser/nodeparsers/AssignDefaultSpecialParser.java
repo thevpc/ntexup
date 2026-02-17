f2a41e5d44ae8a9d1448eec868d76181b5772832
thevpc <taha.bensalah@gmail.com>
2026-02-17 01:21:53 +0100

    Update AssignDefaultSpecialParser.java : fix-syle

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/nodeparsers/AssignDefaultSpecialParser.java
@@ -0,0 +1,18 @@
+package net.thevpc.ntexup.engine.parser.nodeparsers;
+
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.eval.NTxResolutionContext;
+import net.thevpc.ntexup.engine.parser.NTxNodeParserBase;
+
+public class AssignDefaultSpecialParser extends NTxNodeParserBase {
+    public AssignDefaultSpecialParser() {
+        super(true, NTxNodeType.CTRL_ASSIGN_DEFAULT);
+    }
+
+    @Override
+    public void compileNode(NTxNode node, NTxResolutionContext context) {
+
+    }
+}
+
