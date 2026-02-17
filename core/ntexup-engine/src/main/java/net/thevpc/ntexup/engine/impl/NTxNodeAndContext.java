cd3e98a2b4621780fca4f7d43b886cf6271df606
thevpc <taha.bensalah@gmail.com>
2026-02-17 01:21:53 +0100

    Update NTxNodeAndContext.java : fix-syle

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/impl/NTxNodeAndContext.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/impl/NTxNodeAndContext.java
@@ -1,6 +1,8 @@
 package net.thevpc.ntexup.engine.impl;
 
+import net.thevpc.ntexup.api.document.NTxDocument;
 import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.engine.NTxEngine;
 import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 
 public class NTxNodeAndContext {
@@ -10,5 +12,18 @@ public class NTxNodeAndContext {
     public NTxNodeAndContext(NTxNode node, NTxResolutionContext context) {
         this.node = node;
         this.context = context;
+    }
+
+    public void run(NTxDocument document,NTxEngine engine){
+        context.doWithChild(node,
+                cc->engine.compileNode(node, document, cc, new CompileNodeVisitorRunner())
+        );
+    }
+    @Override
+    public String toString() {
+        return "NTxNodeAndContext{" +
+                "node=" + node +
+                ", context=" + context +
+                '}';
     }
 }
