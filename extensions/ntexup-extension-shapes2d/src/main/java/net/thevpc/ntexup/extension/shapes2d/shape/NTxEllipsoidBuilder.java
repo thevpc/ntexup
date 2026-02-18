95499a1758bed23e50956d1990ad3c3e50d36872
thevpc <taha.bensalah@gmail.com>
2026-02-18 10:02:09 +0100

    Update NTxEllipsoidBuilder.java : fix-ntexup

--- extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/shape/NTxEllipsoidBuilder.java
+++ extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/shape/NTxEllipsoidBuilder.java
@@ -27,7 +27,7 @@ public class NTxEllipsoidBuilder implements NTxNodeBuilder {
     public void renderMain(NTxRendererContext rendererContext) {
         NTxNode node = rendererContext.node();
         rendererContext = rendererContext.withDefaultStyles(defaultStyles);
-        NTxBounds2D b = rendererContext.selfBounds(null, null);
+        NTxBounds2D b = rendererContext.selfBounds();
         double x = b.getX();
         double y = b.getY();
         NTxGraphics g = rendererContext.graphics();
