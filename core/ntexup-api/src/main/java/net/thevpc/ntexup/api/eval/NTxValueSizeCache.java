27949894d58501b68bfa84690d5dab135ff9741a
thevpc <taha.bensalah@gmail.com>
2026-02-18 10:02:09 +0100

    Update NTxValueSizeCache.java : fix-ntexup

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxValueSizeCache.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxValueSizeCache.java
@@ -7,9 +7,14 @@ import net.thevpc.ntexup.api.util.NtxFontInfo;
 public class NTxValueSizeCache {
     public NTxElemNumber2 position;
     public NTxElemNumber2 origin;
+    public NTxElemNumber2 innerPosition;
+    public NTxElemNumber2 innerOrigin;
     public boolean preserveRatio;
     public NtxFontInfo fontInfo;
-    public NTxDouble2 componentSize;
+    public NTxDouble2 allowedComponentSize;
+    public NTxDouble2 referenceComponentSize;
+//    public NTxDouble2 componentSize;
+//    public NTxBounds2D componentBounds;
     public NTxMargin margin;
     public NTxSizeRef parentWithMarginRef;
     public NTxBounds2D parentBoundsWithMargin;
