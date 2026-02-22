04c2cc73bf0661237b29f4daccd1142972933583
thevpc <taha.bensalah@gmail.com>
2026-02-22 16:23:16 +0100

    Update NTxCompiledPage.java : first-ajor-fix-ntexup

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxCompiledPage.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxCompiledPage.java
@@ -1,11 +1,14 @@
 package net.thevpc.ntexup.api.engine;
 
 import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 
 public interface NTxCompiledPage {
     int index();
 
     NTxCompiledDocument document();
+
+    NTxResolutionContext pageContext();
 
     NTxNode compiledPage();
 
