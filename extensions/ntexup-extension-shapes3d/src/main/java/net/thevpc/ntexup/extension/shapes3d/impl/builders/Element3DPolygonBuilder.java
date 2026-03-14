b6296920855832f622909686a7d45624a88fc92f
thevpc <taha.bensalah@gmail.com>
2026-03-14 14:44:33 +0100

    [ADDED] added support for meter based values

--- extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/impl/builders/Element3DPolygonBuilder.java
+++ extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/impl/builders/Element3DPolygonBuilder.java
@@ -26,28 +26,29 @@ import java.util.stream.Collectors;
 public class Element3DPolygonBuilder implements  NtxElement3DNodeParser {
     @Override
     public List<String> getId3d() {
-        return Arrays.asList(NTxNodeType.LINE);
+        return Arrays.asList(NTxNodeType.POLYGON);
     }
 
     @Override
-    public NtxElement3D createElement3D(NTxNode node, NTxRendererContext rendererContext, NTxBounds2D b, RealToRelativeMapper mapper, NtxElement3DNodeParserFactory parserFactory) {
-        NTxPoint3D position = NTx3DUtils.asPoint3D(NTxValue.of(node.getPropertyValue(NTxPropName.POSITION))).orNull();
-        Double thickness = NTxValue.of(node.getPropertyValue("thickness")).asDouble().orNull();
-        NTxPoint2D[] points2d = NTx2DUtils.asPoint2DArray(node.getPropertyValue(NTxPropName.POINTS).orNull()).orNull();
+    public NtxElement3D createElement3D(NTxRendererContext rendererContext, NTxBounds2D b, RealToRelativeMapper mapper, NtxElement3DNodeParserFactory parserFactory) {
+        NTxNode node=rendererContext.node();
+        NTxPoint3D position = NtxShapes3dUtils.resolvePosition3D(node, NTxPropName.POSITION, rendererContext, b).orElse(NTxPoint3D.ofZero());
+        double thickness = Math.abs(NtxShapes3dUtils.resolveZDistance(node, "thickness", rendererContext, b).orElse(0.0));
+        NTxPoint2D[] points2d = NtxShapes3dUtils.resolvePositions2D(node,NTxPropName.POINTS,rendererContext,b).orNull();
         if(points2d!=null  && points2d.length>0){
             NTxPoint2D[][] holes = NTx2DUtils.asPoint2DArray2Or1(node.getPropertyValue("holes").orNull()).orElse(new NTxPoint2D[0][]);
             return new NtxElement3DPrism(
                     position,
                     new NTxPolygonWithHoles2DImpl(new NTxRing2DImpl(Arrays.asList(points2d)), Arrays.asList(holes).stream().map(x->new NTxRing2DImpl(Arrays.asList(x))).collect(Collectors.toList())),
-                    thickness==null?0:thickness
+                    thickness
             );
         }
-        NTxPoint3D[] points3d = NtxShapes3dUtils.resolvePoints(node, NTxPropName.POINTS, "real-points", () -> new NTxPoint3D[0], b, mapper);
+        NTxPoint3D[] points3d = NtxShapes3dUtils.resolvePositions3D(node, NTxPropName.POINTS, rendererContext,b).get();
         boolean fill = NTxValue.ofProp(node, NTxPropName.FILL_BACKGROUND).asBoolean().orElse(true);
         boolean contour = NTxValue.ofProp(node, NTxPropName.DRAW_CONTOUR).asBoolean().orElse(true);
 
-        NtxElement3DPolygon r = new NtxElement3DPolygon(Arrays.stream(points3d).map(x -> NTx3DUtils.convertPoint(x, b)).toArray(NTxPoint3D[]::new), fill, contour);
-        NtxShapes3dUtils.apply3dProps(node, r, rendererContext, b);
+        NtxElement3DPolygon r = new NtxElement3DPolygon(points3d, fill, contour);
+        NtxShapes3dUtils.apply3dProps(node, r, rendererContext, b,fill);
         return r;
     }
 
