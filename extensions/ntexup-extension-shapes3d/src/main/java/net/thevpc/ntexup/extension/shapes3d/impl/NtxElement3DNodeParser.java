71b063dffc115dca328ba0de3bb68d85a7ba8faf
thevpc <taha.bensalah@gmail.com>
2026-03-14 14:44:23 +0100

    [ADDED] added support for meter based values

--- extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/impl/NtxElement3DNodeParser.java
+++ extensions/ntexup-extension-shapes3d/src/main/java/net/thevpc/ntexup/extension/shapes3d/impl/NtxElement3DNodeParser.java
@@ -2,6 +2,8 @@ package net.thevpc.ntexup.extension.shapes3d.impl;
 
 import net.thevpc.ntexup.api.document.elem2d.NTxBounds2D;
 import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
+import net.thevpc.ntexup.api.parser.NTxNodeParser;
 import net.thevpc.ntexup.api.renderer.NTxRendererContext;
 import net.thevpc.ntexup.extension.shapes3d.impl.builders.NtxElement3DNodeParserFactory;
 import net.thevpc.ntexup.lib.geometry3d.NtxElement3D;
@@ -11,5 +13,5 @@ import java.util.List;
 
 public interface NtxElement3DNodeParser extends NComponent {
     List<String> getId3d();
-    NtxElement3D createElement3D(NTxNode node, NTxRendererContext rendererContext, NTxBounds2D b, RealToRelativeMapper mapper, NtxElement3DNodeParserFactory parserFactory);
+    NtxElement3D createElement3D(NTxRendererContext rendererContext, NTxBounds2D b, RealToRelativeMapper mapper, NtxElement3DNodeParserFactory parserFactory);
 }
