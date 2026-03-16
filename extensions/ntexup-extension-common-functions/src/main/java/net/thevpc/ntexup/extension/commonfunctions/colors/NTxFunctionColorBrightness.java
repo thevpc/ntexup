86d5407d3a5cf5765fd594702063a1ec0b7b927d
thevpc <taha.bensalah@gmail.com>
2026-03-16 04:58:33 +0100

    Update NTxFunctionColorBrightness.java : wip-resolve-node-by-name

--- extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionColorBrightness.java
+++ extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionColorBrightness.java
@@ -1,13 +1,11 @@
 package net.thevpc.ntexup.extension.commonfunctions.colors;
 
-import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 import net.thevpc.ntexup.api.extension.NTxFunction;
-import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
+import net.thevpc.ntexup.api.eval.NTxFunctionCallContext;
 import net.thevpc.ntexup.api.eval.NTxValue;
 import net.thevpc.ntexup.api.util.NTxElementUtils;
 import net.thevpc.ntexup.extension.commonfunctions.util.NTxColorUtils;
 import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.text.NMsg;
 
 import java.awt.*;
 
@@ -18,19 +16,19 @@ public class NTxFunctionColorBrightness implements NTxFunction {
     }
 
     @Override
-    public NElement invoke(NTxFunctionArgs args, NTxResolutionContext context) {
+    public NElement invoke(NTxFunctionCallContext args) {
         if (args.checkTooFewArgs(1)) {
             return NElement.ofNull();
         }
         args.checkTooManyArgs(2);
-        Color c = args.eval(0, x -> NTxValue.of(x).asColor(), "color", null);
+        Color c = args.evalArg(0, x -> NTxValue.of(x).asColor(), "color", null);
         if(c==null){
             return NElement.ofNull();
         }
         if (args.size() == 1) {
             return NTxElementUtils.toElement(c);
         }
-        float degrees = args.eval(1, x -> NTxValue.of(x).asFloat(), "degrees", () -> 0.0f);
+        float degrees = args.evalArg(1, x -> NTxValue.of(x).asFloat(), "degrees", () -> 0.0f);
         return NTxElementUtils.toElement(NTxColorUtils.adjustBrightness(c, degrees));
 
     }
