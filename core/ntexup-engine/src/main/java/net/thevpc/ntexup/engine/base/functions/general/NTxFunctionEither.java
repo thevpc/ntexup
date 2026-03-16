3c5811f30a560e2244c83da50bcf3f1911a4694d
thevpc <taha.bensalah@gmail.com>
2026-03-16 04:58:33 +0100

    Update NTxFunctionEither.java : wip-resolve-node-by-name

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/general/NTxFunctionEither.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/general/NTxFunctionEither.java
@@ -1,9 +1,7 @@
 package net.thevpc.ntexup.engine.base.functions.general;
 
-import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 import net.thevpc.ntexup.api.extension.NTxFunction;
-import net.thevpc.ntexup.api.eval.NTxFunctionArg;
-import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
+import net.thevpc.ntexup.api.eval.NTxFunctionCallContext;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NBlankable;
 
@@ -14,9 +12,9 @@ public class NTxFunctionEither implements NTxFunction {
     }
 
     @Override
-    public NElement invoke(NTxFunctionArgs args, NTxResolutionContext context) {
+    public NElement invoke(NTxFunctionCallContext args) {
         for (int i = 0; i < args.size(); i++) {
-            NElement u = args.eval(i);
+            NElement u = args.evalArg(i);
             if (i < args.size() - 1 && u.isName() && u.equals(args.arg(i).src())) {
                 // this is a var that could not be resolved, just skip
                 continue;
