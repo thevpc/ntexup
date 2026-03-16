cab085cff873cb18a06c790f05d63bc25e66a458
thevpc <taha.bensalah@gmail.com>
2026-03-16 04:58:33 +0100

    Update NTxFunctionArrayRightRotate.java : wip-resolve-node-by-name

--- extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/general/NTxFunctionArrayRightRotate.java
+++ extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/general/NTxFunctionArrayRightRotate.java
@@ -1,13 +1,9 @@
 package net.thevpc.ntexup.extension.commonfunctions.general;
 
-import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 import net.thevpc.ntexup.api.eval.NTxValue;
 import net.thevpc.ntexup.api.extension.NTxFunction;
-import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
-import net.thevpc.ntexup.api.util.NTxUtils;
+import net.thevpc.ntexup.api.eval.NTxFunctionCallContext;
 import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.text.NMsg;
-import net.thevpc.nuts.util.NOptional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,16 +17,16 @@ public class NTxFunctionArrayRightRotate implements NTxFunction {
     }
 
     @Override
-    public NElement invoke(NTxFunctionArgs args, NTxResolutionContext context) {
+    public NElement invoke(NTxFunctionCallContext args) {
         if (args.checkTooFewArgs(1)) {
             return NElement.ofNull();
         }
         args.checkTooManyArgs(2);
-        NElement[] c = args.eval(0, x -> NTxValue.of(x).asElementArray(), "array", null);
+        NElement[] c = args.evalArg(0, x -> NTxValue.of(x).asElementArray(), "array", null);
         if (c == null || c.length == 0) {
             return NElement.ofNull();
         }
-        Number n = args.size() == 1 ? 1 : args.eval(1, x -> NTxValue.of(x).asNumber(), "number", () -> 1);
+        Number n = args.size() == 1 ? 1 : args.evalArg(1, x -> NTxValue.of(x).asNumber(), "number", () -> 1);
         List<NElement> list = new ArrayList<>(Arrays.asList(c));
         Collections.rotate(list, n.intValue());
         return NElement.ofArray(list.toArray(new NElement[0]));
