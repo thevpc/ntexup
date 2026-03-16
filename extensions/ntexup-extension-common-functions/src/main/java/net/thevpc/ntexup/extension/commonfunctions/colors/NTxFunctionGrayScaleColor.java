b364ec881ea7f66188369d6955f51f87b0cc2b2c
thevpc <taha.bensalah@gmail.com>
2026-03-16 04:58:33 +0100

    Update NTxFunctionGrayScaleColor.java : wip-resolve-node-by-name

--- extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionGrayScaleColor.java
+++ extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionGrayScaleColor.java
@@ -2,7 +2,7 @@ package net.thevpc.ntexup.extension.commonfunctions.colors;
 
 import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 import net.thevpc.ntexup.api.extension.NTxFunction;
-import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
+import net.thevpc.ntexup.api.eval.NTxFunctionCallContext;
 import net.thevpc.ntexup.api.eval.NTxValue;
 import net.thevpc.ntexup.api.util.NTxElementUtils;
 import net.thevpc.ntexup.extension.commonfunctions.util.NTxColorUtils;
@@ -18,14 +18,15 @@ public class NTxFunctionGrayScaleColor implements NTxFunction {
     }
 
     @Override
-    public NElement invoke(NTxFunctionArgs args, NTxResolutionContext context) {
+    public NElement invoke(NTxFunctionCallContext args) {
+        NTxResolutionContext context = args.scopedContext();
         if (args.size() == 0) {
             return NElement.ofNull();
         }
         if (args.size() > 1) {
             context.log().log(NMsg.ofC("%s: expected 1 argument, got %s",NMsg.ofStyledKeyword(name()), args.size()));
         }
-        Color c = NTxValue.of(args.eval(0)).asColor().get();
+        Color c = NTxValue.of(args.evalArg(0)).asColor().get();
         if(c==null){
             return NElement.ofNull();
         }
