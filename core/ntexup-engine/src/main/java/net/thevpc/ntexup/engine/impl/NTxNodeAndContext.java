6d60578151d5924d56dc05751b664f3e35b38f1b
thevpc <taha.bensalah@gmail.com>
2026-03-16 04:58:33 +0100

    Update NTxNodeAndContext.java : wip-resolve-node-by-name

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/impl/NTxNodeAndContext.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/impl/NTxNodeAndContext.java
@@ -2,6 +2,8 @@ package net.thevpc.ntexup.engine.impl;
 
 import net.thevpc.ntexup.api.document.NTxDocument;
 import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.engine.NTxCompiledDocument;
+import net.thevpc.ntexup.api.engine.NTxCompiledPage;
 import net.thevpc.ntexup.api.engine.NTxEngine;
 import net.thevpc.ntexup.api.eval.NTxResolutionContext;
 
@@ -14,11 +16,17 @@ public class NTxNodeAndContext {
         this.context = context;
     }
 
-    public void run(NTxDocument document,NTxEngine engine){
+    public void run(NTxDocument document, NTxEngine engine, NTxCompiledDocument compiledDocument, NTxCompiledPage compiledPage, boolean inPage) {
+        NTxResolutionContext context = this.context;
+        if (inPage) {
+            context = context.copy();
+            context.setInPage(true);
+        }
         context.doWithChild(node,
-                cc->engine.compileNode(node, document, cc, new CompileNodeVisitorRunner())
+                cc -> engine.compileNode(node, document, compiledDocument, compiledPage, cc, new CompileNodeVisitorRunner())
         );
     }
+
     @Override
     public String toString() {
         return "NTxNodeAndContext{" +
