b82ba464cf0918f612a08aa6a199d9e876d541d9
thevpc <taha.bensalah@gmail.com>
2026-03-16 04:58:33 +0100

    Update NTxFunctionSinebowColor.java : wip-resolve-node-by-name

--- extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionSinebowColor.java
+++ extensions/ntexup-extension-common-functions/src/main/java/net/thevpc/ntexup/extension/commonfunctions/colors/NTxFunctionSinebowColor.java
@@ -1,13 +1,11 @@
 package net.thevpc.ntexup.extension.commonfunctions.colors;
 
-import net.thevpc.ntexup.api.eval.NTxResolutionContext;
-import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
+import net.thevpc.ntexup.api.eval.NTxFunctionCallContext;
 import net.thevpc.ntexup.api.eval.NTxValue;
 import net.thevpc.ntexup.api.extension.NTxFunction;
 import net.thevpc.ntexup.api.util.NTxElementUtils;
 import net.thevpc.ntexup.extension.commonfunctions.util.NTxColorUtils;
 import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.text.NMsg;
 
 public class NTxFunctionSinebowColor implements NTxFunction {
     @Override
@@ -16,12 +14,12 @@ public class NTxFunctionSinebowColor implements NTxFunction {
     }
 
     @Override
-    public NElement invoke(NTxFunctionArgs args, NTxResolutionContext context) {
+    public NElement invoke(NTxFunctionCallContext args) {
         if (args.size() == 0) {
             return NElement.ofNull();
         }
         args.checkTooManyArgs(1);
-        float ratio = args.eval(0, x -> NTxValue.of(x).asFloat(), "ratio", () -> 0.0f);
+        float ratio = args.evalArg(0, x -> NTxValue.of(x).asFloat(), "ratio", () -> 0.0f);
         return NTxElementUtils.toElement(NTxColorUtils.sinebow(ratio));
     }
 }
