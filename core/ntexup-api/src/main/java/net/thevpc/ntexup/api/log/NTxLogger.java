2bb014e65b324004f51a019a8f366b9bdb26a81c
thevpc <taha.bensalah@gmail.com>
2026-03-21 19:40:19 +0100

    Update NTxLogger.java : -m

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/log/NTxLogger.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/log/NTxLogger.java
@@ -2,11 +2,11 @@ package net.thevpc.ntexup.api.log;
 
 
 import net.thevpc.ntexup.api.source.NTxSource;
+import net.thevpc.nuts.log.NLogger;
 import net.thevpc.nuts.text.NMsg;
 
-public interface NTxLogger {
+public interface NTxLogger extends NLogger {
 
     void log(NTxMsg message);
-    void log(NMsg message);
     void log(NMsg message, NTxSource source);
 }
