3165878561161ab99152b9b58bf419d281f71159
thevpc <taha.bensalah@gmail.com>
2026-03-26 13:50:39 +0100

    Update NTxCompiledPage.java : update

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxCompiledPage.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxCompiledPage.java
@@ -2,10 +2,12 @@ package net.thevpc.ntexup.api.engine;
 
 import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.eval.NTxResolutionContext;
+import net.thevpc.nuts.elem.NElement;
 
 public interface NTxCompiledPage {
     int index();
 
+    NElement toElement(boolean semantic);
     NTxCompiledDocument document();
 
     NTxResolutionContext pageContext();
