e426dcac820e322e7252ca9385d1716910c1585c
thevpc <taha.bensalah@gmail.com>
2026-03-26 13:50:39 +0100

    Update NTxCompiledDocument.java : update

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxCompiledDocument.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxCompiledDocument.java
@@ -1,7 +1,12 @@
 package net.thevpc.ntexup.api.engine;
 
 import net.thevpc.ntexup.api.document.NTxDocument;
+import net.thevpc.ntexup.api.document.security.NTxManifest;
+import net.thevpc.ntexup.api.document.security.NTxManifestOptions;
+import net.thevpc.ntexup.api.eval.NTxObj;
 import net.thevpc.ntexup.api.source.NTxSource;
+import net.thevpc.ntexup.api.source.NTxSourceMonitor;
+import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NOptional;
 
 import java.util.Iterator;
@@ -10,7 +15,7 @@ import java.util.List;
 public interface NTxCompiledDocument {
     NTxSource source();
 
-    NTxDocument compiledDocument();
+    NTxDocument document();
 
     boolean isCompiled();
 
@@ -23,7 +28,18 @@ public interface NTxCompiledDocument {
     Iterator<NTxCompiledPage> pagesIterator();
 
     NOptional<NTxCompiledPage> page(int index);
+
     List<NTxCompiledPage> pages();
 
     Throwable currentThrowable();
+
+    NElement toElement(boolean semantic);
+
+    NTxSourceMonitor sourceMonitor();
+
+    NTxManifest computeManifest(NTxManifestOptions options);
+
+    NOptional<NTxObj> getGlobalObject(String name);
+
+    NTxCompiledDocument setGlobalObject(String name, NTxObj obj);
 }
