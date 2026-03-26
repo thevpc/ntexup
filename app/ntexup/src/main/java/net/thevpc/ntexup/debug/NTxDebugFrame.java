18373159a7e4da2a172546c00bc1bf3859d71f63
thevpc <taha.bensalah@gmail.com>
2026-03-26 13:50:39 +0100

    Update NTxDebugFrame.java : update

--- app/ntexup/src/main/java/net/thevpc/ntexup/debug/NTxDebugFrame.java
+++ app/ntexup/src/main/java/net/thevpc/ntexup/debug/NTxDebugFrame.java
@@ -4,8 +4,6 @@ import net.thevpc.ntexup.api.engine.NTxCompiledDocument;
 import net.thevpc.ntexup.api.engine.NTxCompiledPage;
 import net.thevpc.ntexup.api.engine.NTxEngine;
 import net.thevpc.ntexup.api.log.NTxLogger;
-import net.thevpc.ntexup.api.renderer.NTxDocumentView;
-import net.thevpc.ntexup.api.renderer.NTxDocumentViewManager;
 import net.thevpc.ntexup.engine.util.NTxUtilsImages;
 import net.thevpc.ntexup.api.renderer.NTxDocumentRendererListener;
 import net.thevpc.ntexup.api.renderer.NTxDocumentStreamRendererConfig;
@@ -15,7 +13,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.util.ArrayList;
 
 public class NTxDebugFrame extends JFrame {
     private NTxDebugPanel debugPanel;
@@ -24,7 +21,7 @@ public class NTxDebugFrame extends JFrame {
     NTxDocumentRendererListener hDocumentRendererListener = new NTxDocumentRendererListener() {
         @Override
         public void onChangedCompiledDocument(NTxCompiledDocument compiledDocument) {
-            model().setCompiledDocument(compiledDocument.compiledDocument());
+            model().setCompiledDocument(compiledDocument.document());
             model().setRawDocument(compiledDocument.rawDocument());
             updateContent();
         }
